0c7eacdc4d9ca6f2ba0449c5ae408b2d7aa39bf9
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-11 00:54:25 +0530

    Time: 20 ms (57.14%), Space: 17.3 MB (12.53%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+bool Find_Path(TreeNode* root,vector<TreeNode*> &path, TreeNode* n)
+{
+    if(root==NULL)
+        return false;
+    
+    path.push_back(root);
+    
+    if(root->val==n->val)
+        return true;
+    
+    
+   if(!Find_Path(root->left,path,n) &&  !Find_Path(root->right,path,n))
+   {
+       path.pop_back();
+       return false;
+   }
+    else
+        return true;
+}
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        if(root==NULL)
+            return NULL;
+        
+        vector<TreeNode*> Path1, Path2;
+        
+        Find_Path(root,Path1,p);
+        Find_Path(root,Path2,q);
+        
+//         for(int i=0;i<Path1.size();i++)
+//             cout<<Path1[i]->val<<" ";
+        
+//         cout<<"\n";
+        
+//         for(int i=0;i<Path2.size();i++)
+//             cout<<Path2[i]->val<<" ";
+        
+        // cout<<"\n";
+        
+        Path1.push_back(NULL);
+        Path2.push_back(NULL);
+        
+        for(int i=0;i<Path1.size()-1 && i<Path2.size()-1;i++)
+        {
+            if(Path1[i+1]!=Path2[i+1])
+                return Path1[i];
+        }
+        
+        return NULL;
+        
+    }
+};
