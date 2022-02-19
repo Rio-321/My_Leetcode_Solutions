b70de0b4ca178cd1eb0535c5d647fd145badcb37
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-19 20:22:16 +0530

    Time: 26 ms (29.88%), Space: 13 MB (95.67%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -7,29 +7,79 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         
-        unordered_set<int> row_No, col_No;
+        bool is_row = false, is_col = false;
+        
+        // unordered_set<int> row_No, col_No;
+        
+        for(int i=0;i<n;i++)
+            if(matrix[0][i]==0)
+            {
+                is_row = true;
+                break;
+            }
+                
         
         for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
+            if(matrix[i][0]==0)
             {
+                is_col = true;
+                break;
+            }
+                
+        
+        
+        for(int i=0;i<m;i++)
+        {  
+            for(int j=0;j<n;j++)
+            {  
                 if(matrix[i][j]==0)
-                {
-                    row_No.insert(i);
-                    col_No.insert(j);
+                {   
+                    // if(i==0)
+                        // is_row = true;
+                    
+                    
+                    // row_No.insert(i);
+                    // col_No.insert(j);
+                    
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
                     // zero_list.push_back(i*n+j);  
             }
         
-        
-        
-        for(int i=0;i<m;i++)
+        }
+            
+         
+        for(int i=1;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n;j++)
             {
-                if(row_No.find(i)!=row_No.end() || col_No.find(j)!=col_No.end())
+                if(matrix[i][0]==0 || matrix[0][j]==0)
                     matrix[i][j] = 0;
             }
         }
+        
+        if(is_row)
+        {
+            for(int i=0;i<n;i++)
+                matrix[0][i] = 0;
+        }
+
+        if(is_col)
+        {
+            for(int i=0;i<m;i++)
+                matrix[i][0] = 0;
+        }
+
+        
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(row_No.find(i)!=row_No.end() || col_No.find(j)!=col_No.end())
+        //             matrix[i][j] = 0;
+        //     }
+        // }
         
         
 //         for(int x:zero_list)
