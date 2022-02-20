0688c732d5d5333d28453f4db777ace6a9af3965
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-20 17:31:23 +0530

    Time: 20 ms (74.19%), Space: 20.4 MB (23.28%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -4,62 +4,29 @@ public:
         
         int len = flowerbed.size();
         int count = 0;
-        bool prev = false, next = false;
         for(int i=0;i<len;i++)
         {  
-           prev = false;
-           next = false;
+           bool prev = false;
+           bool next = false;
             
-            if(count>=n)
-             return true;
-         
-            if(flowerbed[i]==1)
-                continue;
-            
-            // cout<<i<<" ";
-         
-            if(i==0)
-                prev = true;
-            else if(i>0)
+            if(flowerbed[i]==0)
             {
-                if(flowerbed[i-1]==0)
+                 if(i==0 || (flowerbed[i-1]==0))
                     prev = true;
-                else
-                    prev = false;
-            }
-         
-         
-            if(i==len-1)
-                next = true;
-            else if(i<len-1)
-            {
-                if(flowerbed[i+1]==0)
+                
+                 if(i==len-1 || (flowerbed[i+1]==0))
                     next = true;
-                else
-                    next = false;
-            }
                 
-                
-            if(prev==true && next==true)
-            {
-                flowerbed[i] = 1;
-                count++;
-                // cout<<i<<" ";
-            }
-                // count++;
-            
-
+                 if(prev && next)
+                    {
+                        flowerbed[i] = 1;
+                        count++;
+                    }  
+            }    
                 
         }
         
-        
-        // cout<<count<<"\n";
-        
-        
-    if(count>=n)
-        return true;
-    else
-        return false;
+    return count>=n;
         
     }
 };
