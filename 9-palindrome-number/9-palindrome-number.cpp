dd30a7bd4ca062f0f7033cf4ade01ad3e36b64ac
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-23 09:43:48 +0530

    Time: 16 ms (62.44%), Space: 6 MB (32.40%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -7,18 +7,31 @@ public:
         
         if(x<10)
             return true;
+        if(x%10==0 && x!=0)
+            return false;
+        
+        int rev_No = 0;
+        
+        while(x>rev_No)
+        {
+            rev_No = rev_No*10 + x%10;
+            x = x/10;
+        }
+        
+        if(x==rev_No || x==rev_No/10)  return true;
+        else return false;
         
         // int digits = floor(log10(x) + 1);
         
-        string str = to_string(x);
-        // cout<<str;
+//         string str = to_string(x);
+//         // cout<<str;
         
-        int n = str.length();
-        for(int i=0;i<n/2;i++)
-        {
-            if(str[i]!=str[n-1-i])
-                return false;
-        }
+//         int n = str.length();
+//         for(int i=0;i<n/2;i++)
+//         {
+//             if(str[i]!=str[n-1-i])
+//                 return false;
+//         }
         
         return true;
         
