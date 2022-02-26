11b901d4ece330c615b998da35376c5f4a8770f7
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-26 08:33:47 +0530

    Time: 4 ms (19.62%), Space: 5.9 MB (16.77%) - LeetHub

---
+++ 292-nim-game/292-nim-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        
+        // if(n<=3)
+        //     return true;
+        // if(n==4)
+        //     return false;
+        
+        if(n%4==0)
+            return false;
+        
+        return true;
+        
+    }
+};
