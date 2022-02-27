7298d022da8a0bf5ed5bc666d7b23d723b796e31
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-02-27 21:38:19 +0530

    Time: 188 ms (99.63%), Space: 100.9 MB (49.06%) - LeetHub

--- 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
+++ 2171-removing-minimum-number-of-magic-beans/2171-removing-minimum-number-of-magic-beans.cpp
@@ -15,49 +15,20 @@ public:
             sum = sum + beans[i];
         
         long long r = n;
-        long long res = LLONG_MAX;
+        long long res = LLONG_MIN;
         
-        // cout<<sum<<"\n";
         
         for(int i=0;i<n;i++)
         { 
-            long long p = sum - r*beans[i];
+            long long p = r*beans[i];
             // cout<<p<<" ";
             
-            res = min(res, p);
+            res = max(res, p);
             r--;
         }
+        
+        res = sum - res;
                       
-        
-        
-            
-//         for(int i=0;i<n;i++)
-//             {
-//                 if(i==0)
-//                     pre_sum[i] = beans[i];
-//                 else
-//                     pre_sum[i] = pre_sum[i-1] + beans[i];
-//             }
-        
-//         for(int i=n-1;i>=0;i--)
-//         {
-//             if(i==n-1)
-//                 suff_sum[i] = beans[i];
-//             else
-//                 suff_sum[i] = suff_sum[i+1]+beans[i];
-//         }
-        
-        
-        
-//         for(int i=0;i<n;i++)
-//         {
-//             long long p = pre_sum[i] - beans[i] + suff_sum[i] - (long long)(n)*beans[i] + (long long)(i)*beans[i];
-//             cout<<p<<" ";
-//             res = min(res,p);
-//         }
-        
-//         // cout<<"\n";
-        
         return res;
     }
 };
