d2aed3d0c7fc2ecb6eee3022cf5302751273a5ea
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-01 12:10:51 +0530

    Time: 54 ms (49.69%), Space: 27.5 MB (63.91%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int Index_Of(vector<int>& nums, int x, int low, int high)
+    {
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            
+            if(nums[mid]==x)
+                return mid;
+            
+            if(nums[mid]>x)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        
+        return -1;        
+    }
+    
+    int search(vector<int>& nums, int target) {
+        
+        return Index_Of(nums,target,0,nums.size()-1);
+        
+    }
+};
