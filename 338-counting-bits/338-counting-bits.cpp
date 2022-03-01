55120e6e07028af1cdcb25d5b8842dbd5bca25c5
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-02 01:38:12 +0530

    Time: 6 ms (65.52%), Space: 7.7 MB (98.04%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> res(n+1,0);
+        
+        if(n==0) return res;
+        
+        res[0] = 0;
+        res[1] = 1;
+        
+        for(int i=2;i<=n;i++)
+        {   
+            if((i%2) == 0) //even
+                res[i] = res[i/2];
+            else
+                res[i] = res[i/2] + 1;
+        }
+        
+        
+        return res;
+        
+    }
+};
