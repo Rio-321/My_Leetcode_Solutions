928c976e408e6c940f547f9f8aa266accecfb9d3
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-04 11:41:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.95%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,56 +1,56 @@
 class Solution {
 public:
-    double f(double x, long long n)
-    {
-        if(n==0) return 1.0;
-        if(n==1) return x;
+//     double f(double x, long long n)
+//     {
+//         if(n==0) return 1.0;
+//         if(n==1) return x;
         
-        if(n%2==0)
-            return f(x*x,n/2);
-        else
-            return x*f(x,n-1);
-    }
+//         if(n%2==0)
+//             return f(x*x,n/2);
+//         else
+//             return x*f(x,n-1);
+//     }
     
     double myPow(double x, int n) {
         
         
-        long long num = n;
-        if(num<0)
-        {
-           num = -1*num;
-            x = 1/x;
-        }
-                        
-        return f(x, num);
-        
-        
-        
-        //Approach 2
-        
 //         long long num = n;
-//         double ans = 1.0;
-//         if(num<0) 
-//             num = -1*num;
-        
-        
-//         while(num>0)
+//         if(num<0)
 //         {
-//             if(num%2==1)
-//             {
-//                 ans = ans * x;
-//                 num-=1;
-//             }
-//             else
-//             {
-//                 x = x*x;
-//                 num = num/2;
-//             }
+//            num = -1*num;
+//             x = 1/x;
 //         }
+                        
+//         return f(x, num);
         
         
-//         if(n<0) ans = 1.0/ans;
         
-//         return ans;
+        // Approach 2
+        
+        long long num = n;
+        double ans = 1.0;
+        if(num<0) 
+            num = -1*num;
+        
+        
+        while(num>0)
+        {
+            if(num%2==1)
+            {
+                ans = ans * x;
+                num-=1;
+            }
+            else
+            {
+                x = x*x;
+                num = num/2;
+            }
+        }
+        
+        
+        if(n<0) ans = 1.0/ans;
+        
+        return ans;
         
         
     }
