e597eec48d362ff761c42b5cc4a0519714d5ae2d
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-04 13:38:55 +0530

    Time: 10 ms (48.88%), Space: 13.9 MB (11.29%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -5,6 +5,8 @@ public:
     {
         if(dp[x]!=-1) return dp[x];
         
+        if(x==0||x==1) return 0;
+        
         dp[x] = min(f(x-1,cost,dp)+cost[x-1], f(x-2,cost,dp)+cost[x-2]);
         
         return dp[x];
@@ -13,8 +15,8 @@ public:
         
         int n = cost.size();
         vector<int> dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 0;
+        // dp[0] = 0;
+        // dp[1] = 0;
         return f(n,cost,dp);
     }
 };
