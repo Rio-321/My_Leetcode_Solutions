d37b7ad30958fd59b7a72ad5fdbbe35666e787a5
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-06 18:09:29 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (47.38%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,7 +3,6 @@ public:
     int rob(vector<int>& nums) {
         
         int n = nums.size();
-        // vector<int> dp(n);
         
         int curr,f=nums[0];
         
@@ -12,13 +11,8 @@ public:
         int s=nums[1];
         if(n==2) return max(f, s);
         
-        // dp[0] = nums[0];
-        // dp[1] = max(nums[1],nums[0]);
-        
-        
         int max_num = nums[0];
-        
-        
+    
         for(int i=2;i<n;i++)
         {  
             curr = max_num + nums[i];
@@ -27,11 +21,6 @@ public:
             s = curr;
             
         }
-        
-        // for(int x:dp)
-        //     cout<<x<<" ";
-        
-        // return max(dp[n-2], dp[n-1]);
         return max(f,s);        
     }
 };
