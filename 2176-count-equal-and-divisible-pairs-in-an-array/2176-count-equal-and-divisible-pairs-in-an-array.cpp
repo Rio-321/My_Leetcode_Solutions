95eee8fe3257fdb9c118b33207f555ab31bbe6ad
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-03-08 16:21:17 +0530

    Time: 21 ms (25.88%), Space: 13.5 MB (5.50%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+   int GCD(int a, int b)
+    {
+        if(b==0) return a;
+        else
+            return GCD(b,a%b);
+    }
+    
     int countPairs(vector<int>& nums, int k) {
         
         unordered_map<int,vector<int>> mp;
@@ -8,17 +15,40 @@ public:
         
         for(int i=0;i<nums.size();i++)
         {
-            if(mp.find(nums[i])!=mp.end())
-            {
-                for(auto x: mp[nums[i]])
-                {
-                    if((x*i)%k == 0)
-                        count++;
-                }
-            }
-            
             mp[nums[i]].push_back(i);
             
+//             if(mp.find(nums[i])!=mp.end())
+//             {
+//                 for(auto x: mp[nums[i]])
+//                 {
+//                     if((x*i)%k == 0)
+//                         count++;
+//                 }
+//             }
+            
+            
+            
+        }
+        
+        
+        vector<int> vec;
+        for(int i=1;i*i<=k;i++)
+        {
+            if(k%i==0) vec.push_back(i);
+            if(k%i==0 && k/i!=i) vec.push_back(k/i);
+        }
+        
+        for(auto x:mp)
+        {
+            unordered_map<int,int> m;
+            for(auto ind:x.second)
+            {   
+                
+                count = count + m[k/GCD(k,ind)];
+                
+                for(auto j:vec)
+                    if(ind%j==0) m[j]++;
+            }
         }
         
         return count;
