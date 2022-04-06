3e61e9faa09caad5663a9af3c8a5ba485286bcee
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-04-06 14:29:51 +0530

    Time: 15 ms (46.03%), Space: 11.6 MB (50.31%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -9,7 +9,8 @@ public:
         {
            int  mid = (l+r)/2;
             if(mid==0) return 1;
-            // if(mid==n-1) return 
+        
+            
             if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]) return mid;
             else
                 if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1])
