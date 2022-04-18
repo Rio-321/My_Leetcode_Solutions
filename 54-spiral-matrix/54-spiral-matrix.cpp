7096607ce024d01043235b68cb36353102cfb36e
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-04-18 13:48:43 +0530

    Time: 0 ms (100.00%), Space: 7 MB (32.12%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,44 +1,56 @@
 class Solution {
 public:
-    void spiral_matrix(vector<vector<int>> &matrix, int m, int n,int i, int j, vector<int>& vec)
-    { 
-        int p=i,q=j;
-        if(i>m || j>n) return;
-        
-        // int i=0,j=0;
-        
-        // cout<<i<<" "<<j<<" "<<m<<" "<<n<<"\n";
-        
-        for(j;j<=n;j++) vec.push_back(matrix[i][j]);
-        j--;
-        i++;
-        for(i;i<=m;i++) vec.push_back(matrix[i][j]);
-        i = m;
-        j = n-1;
-        for(j;i>p && j>=q;j--) vec.push_back(matrix[i][j]);
-        i = m-1;
-        j = q;
-        for(i;i>p && j<n;i--) vec.push_back(matrix[i][j]);
-    
-    
-        
-        // cout<<i<<" "<<j<<"\n";
-        
-        spiral_matrix(matrix,m-1,n-1,p+1,q+1,vec);
-        
-        return;
-        
-    }
+
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         
-        vector<int> vec;
         int m = matrix.size();
         int n = matrix[0].size();
-        spiral_matrix(matrix,m-1,n-1,0,0,vec);
+;        int T = 0,B=m-1,L=0,R=n-1;
+        int dir = 0;
         
-        // for(auto x:vec)
-        //     cout<<x<<" ";
+        vector<int> vec;
+        
+        while(T<=B && L<=R)
+        {
+            if(dir==0)
+            {
+                for(int j=L;j<=R;j++)
+                    vec.push_back(matrix[T][j]);
+                
+                    T++;
+            }
+            
+            if(dir==1)
+            {
+                for(int i=T;i<=B;i++)
+                    vec.push_back(matrix[i][R]);
+                
+                    R--;
+            }
+            
+            if(dir==2)
+            {
+                for(int j=R;j>=L;j--)
+                    vec.push_back(matrix[B][j]);
+                
+                    B--;
+            }
+            
+            if(dir==3)
+            {
+                for(int i=B;i>=T;i--)
+                    vec.push_back(matrix[i][L]);
+                
+                    L++;
+            }
+            
+            dir = (dir+1)%4;
+            
+           
+        }
         
         return vec;
+        
+       
     }
 };
