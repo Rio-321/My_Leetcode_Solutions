b9d5240b2217356ce85fbe7593cceac910d4c0a1
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-04-18 15:41:43 +0530

    Time: 17 ms (100.00%), Space: 6 MB (100.00%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        
+        int fir = max(cost1,cost2);
+        int sec = min(cost1,cost2);
+        
+        int lim = total/fir;
+        long long res = 0;
+        
+        for(int i=0;i<=lim;i++)
+        {
+            res = res + ((total - (long long)(i*fir))/sec + 1);
+        }
+        
+        return res;
+    }
+};
