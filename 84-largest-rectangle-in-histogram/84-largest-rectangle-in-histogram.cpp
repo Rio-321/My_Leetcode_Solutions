d1df3f12cfe7d83073f845228d912c6b43050ef4
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-10 09:48:06 +0530

    Time: 194 ms (75.51%), Space: 77.7 MB (56.87%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -4,7 +4,6 @@ public:
         
         int n = heights.size();
         vector<int> prev(n,0);
-        vector<int> past(n,0);
         
         vector<int> St;
         int area = 0;
@@ -13,18 +12,13 @@ public:
         for(int i=0;i<n;i++)
         {
             while(!St.empty() && (heights[St.back()]>=heights[i]) )
-            {
                 St.pop_back();
-            }
+            
             
             if(St.empty())
-            {
                 prev[i] = -1;
-            }
             else
-            {
                 prev[i] = St.back();
-            }
             
             St.push_back(i);
             
@@ -35,33 +29,17 @@ public:
         for(int i=n-1;i>=0;i--)
         {
             while(!St.empty() && ( heights[St.back()] >= heights[i]) )
-            {
                 St.pop_back();
-            }
             
             if(St.empty())
-            {
-                // past[i] = n;
                 area = (n-prev[i]-1)*heights[i];
-                // res = max()
-            }
             else
                 area = (St.back()-prev[i]-1)*heights[i];
-                // past[i] = St.back();
             
             St.push_back(i);
             res = max(res,area);
         }
         
-
-//         int res = INT_MIN;
-//         for(int i=0;i<n;i++)
-//         {
-//             int len  = past[i]-prev[i]-1;
-//             int area = heights[i]*len;
-            
-//             res = max(res,area);
-//         }
         
         
         return res;
