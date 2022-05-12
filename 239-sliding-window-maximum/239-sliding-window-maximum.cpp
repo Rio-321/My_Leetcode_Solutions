4495ffb793fec1a850631e50918a92dd64fcd885
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-12 10:30:19 +0530

    Time: 610 ms (17.76%), Space: 131.9 MB (74.29%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
-   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       deque<int> queue;
-       vector<int> ans;
-       for (int left = 0; left < nums.size(); ++left) {
-           // As the window move on, element nums[left-k] will be outdated.
-           if (queue.front() == left - k) queue.pop_front();
-           // Now we are ready to push our new element nums[left]'s index into the queue.
-           // But before that, we should clear elements which is smaller then nums[left].
-           // Why? Because if nums[left] is bigger then nums[i], 
-           // there will be no way for nums[i] be selected as the max number in range (left-k, left]
-           while (!queue.empty() && nums[queue.back()] < nums[left]) queue.pop_back();
-           // Now push the index into our queue.
-           queue.push_back(left);
-           // Okay, now nums[queue.front()] mush be the max number in range (left-k, left] 
-           if (left - k + 1 >= 0) ans.push_back(nums[queue.front()]);
-       }
-       return ans;
-   }
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        int i=0,j=0;
+        
+        deque<int> res;
+        
+        vector<int> ans;
+        
+        while(j<nums.size())
+        {   
+
+            
+            while(!res.empty() && nums[res.back()]<nums[j])
+                res.pop_back();
+            
+            res.push_back(j);
+            
+           if(res.front()<i)
+                res.pop_front();
+            
+            
+            
+            if(j-i+1==k)
+            { 
+                ans.push_back(nums[res.front()]); 
+                i++;
+            }
+            
+            j++;
+            
+            
+        }
+        
+        return ans;
+        
+    }
+    
+    
 };
