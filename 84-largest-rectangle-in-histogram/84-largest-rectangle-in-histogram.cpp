c59ec44acff77981a705b303b32edb173023b3c7
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-23 17:02:07 +0530

    Time: 286 ms (27.73%), Space: 77.8 MB (56.93%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -3,46 +3,52 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         
         int n = heights.size();
-        vector<int> prev(n,0);
+        vector<int> dp(n,0);
+        vector<int> st;
+        for(int i=0;i<n;i++)
+        {
+              while(!st.empty() && heights[st.back()]>=heights[i])
+                  st.pop_back();
+            
+            if(st.empty())
+                dp[i] = i+1;
+            else
+                dp[i] = i-st.back();
+            
+            st.push_back(i);
+        }
         
-        vector<int> St;
-        int area = 0;
+//                 for(auto x:dp)
+//             cout<<x<<" ";
+        
+//         cout<<"\n";
+        
+
+        
+        st.clear();
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && heights[st.back()]>=heights[i])
+                st.pop_back();
+            
+            if(st.empty())
+                dp[i] = dp[i] + n-1-i;
+            else
+                dp[i] = dp[i] + st.back()-i-1;
+            
+            st.push_back(i);
+        }
+        
         int res = INT_MIN;
         
         for(int i=0;i<n;i++)
         {
-            while(!St.empty() && (heights[St.back()]>=heights[i]) )
-                St.pop_back();
-            
-            
-            if(St.empty())
-                prev[i] = -1;
-            else
-                prev[i] = St.back();
-            
-            St.push_back(i);
-            
+            res = max(res, dp[i]*heights[i]);
         }
-        
-        St.clear();
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!St.empty() && ( heights[St.back()] >= heights[i]) )
-                St.pop_back();
-            
-            if(St.empty())
-                area = (n-prev[i]-1)*heights[i];
-            else
-                area = (St.back()-prev[i]-1)*heights[i];
-            
-            St.push_back(i);
-            res = max(res,area);
-        }
-        
-        
         
         return res;
+        
         
     }
 };
