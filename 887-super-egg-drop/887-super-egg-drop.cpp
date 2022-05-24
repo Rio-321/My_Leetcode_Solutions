b747742dd250ff66dae74ff8cd6a237ff6048e59
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-24 10:13:22 +0530

    Time: 1809 ms (5.08%), Space: 47.3 MB (16.22%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
-    int superEggDrop(int K, int N) {
+    int superEggDrop(int k, int n) {
+        // k = eggs & n = floors
+        vector<vector<int>> dp(n+1, vector<int>(k+1,0));
+         // dp[i][j] = floor and eggs
         
-    if(N==0||N==1) return N;     //Think for base case
-        if(K==1) return N;
+        // if i==1 return 1
+        for(int j=1;j<=k;j++)
+            dp[1][j] = 1;
         
-        vector<vector<int>> dp(K+1,vector<int> (N+1,0));
-      for(int i=0;i<=K;i++)
-        dp[i][0]=0,dp[i][1]=1;   //Filling from base case as if N==0 ans=0 , N==1 ans=1
-      for(int i=0;i<=N;i++)
-        dp[0][i]=0,dp[1][i]=i;   //Filling from base case as if K==0 ans=0 , K==1 ans=N, number of current floor (i)
+        //j==1 (eggs==1) 
+        for(int i=1;i<=n;i++)
+           dp[i][1] = i;
         
-        for(int i=2;i<K+1;i++)
+        for(int i=2;i<=n;i++)
         {
-            for(int j=2;j<N+1;j++)
-            {    int l=1,h=j,temp=0,ans=100000;
-                 while(l<=h)
-                 {
-                    int mid=(l+h)/2;
-                    int left= dp[i-1][mid-1]; //egg broken check for down floors of mid
-                    int right= dp[i][j-mid];  // not broken check for up floors of mid
-                    temp=1+max(left,right);          //store max of both 
-                    if(left<right){                  //since right is more than left and we need more in worst case 
-                      l=mid+1;                       // so l=mid+1 to gain more temp for worst case : upward
-                    }
-                    else                             //left > right so we will go downward 
-                    {    
-                        h=mid-1;
-                    }
-                    ans=min(ans,temp);               //store minimum attempts
-                 }
-                 dp[i][j]=ans;
+            for(int j=2;j<=k;j++)
+            {
+                dp[i][j] = INT_MAX;
+                int l = 1,h = i;
+                
+                while(l<=h)
+                {
+                    int x = (l+h)/2;
+                    int left = dp[x-1][j-1];
+                    int right = dp[i-x][j];
+                    
+                    int temp = 1 + max(left, right);
+                    
+                    if(left<right)
+                        l = x+1;
+                    else
+                        h = x-1;
+                    
+                    dp[i][j] = min(dp[i][j], temp);
+                    
+                }
+                
+//                 for(int x=1;x<=i;x++)
+//                 {
+//                     int temp = 1 + max(dp[x-1][j-1], dp[i-x][j]);
+                    
+//                     dp[i][j] = min(dp[i][j], temp);
+//                 }
             }
         }
-        return dp[K][N];
          
+        
+        return dp[n][k];
         
     }
 };
