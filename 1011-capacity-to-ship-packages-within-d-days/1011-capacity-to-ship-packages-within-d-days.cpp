74d989b921b48437bc4c726faa0b3a5eaa69d743
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-28 14:54:45 +0530

    Time: 126 ms (8.74%), Space: 26.2 MB (27.51%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    
+    bool is_possible(vector<int> &weights, int days, int mid)
+    {
+        int res = 0;
+        int i=0;
+        int cap = mid;
+        
+        while(i<weights.size())
+        {   
+            if(weights[i]>mid)
+                return false;
+            
+            if(cap>=weights[i])
+            {
+                cap -= weights[i];
+                i++;
+            }
+            else
+            {
+                cap = mid;
+                res++;
+                
+                // if(mid==1)
+                    // cout<<res<<" ";
+            }
+                
+        }
+        res++;
+        
+        if(res<=days) return true;
+        else
+            return false;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        // if day==1  then ans = sum of weights
+        
+        
+        int sum = accumulate(weights.begin(), weights.end(),0);
+        
+        if(days==1) return sum;
+        
+        int low = 1,high = sum;
+        
+        int ans = 0;
+        // cout<<sum<<"\n";
+        
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            // cout<<low<<" "<<mid<<" "<<high<<"\n";
+            
+            
+            if(is_possible(weights,days,mid))
+            {
+                ans = mid;
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        
+        
+        return ans;
+        
+    }
+};
