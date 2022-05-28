eb9453785d26211fd5fd9e3c0a957c1b31b25477
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-28 18:03:52 +0530

    Time: 43 ms (37.97%), Space: 23.3 MB (13.59%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,20 +12,33 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        //if the root is same as p or q
-        if(root->val == p->val || root->val==q->val)
-            return root;
+//         //if the root is same as p or q
+//         if(root->val == p->val || root->val==q->val)
+//             return root;
         
-        // if both are less than curr node
-        if(p->val < root->val && q->val < root->val)
-            return lowestCommonAncestor(root->left,p,q);
+//         // if both are less than curr node
+//         if(p->val < root->val && q->val < root->val)
+//             return lowestCommonAncestor(root->left,p,q);
         
-        //if both are greater than curr node
-        if(p->val > root->val && q->val > root->val)
-            return lowestCommonAncestor(root->right,p,q);
+//         //if both are greater than curr node
+//         if(p->val > root->val && q->val > root->val)
+//             return lowestCommonAncestor(root->right,p,q);
         
-        //one is less and one is greater
-         return root;
+//         //one is less and one is greater
+//          return root;
+        
+        
+        //ANOTHER SOLUTION
+        
+        while((root->val - p->val) * (root->val - q->val)  > 0)
+        {
+            if(root->val > p->val)
+                root = root->left;
+            else
+                root = root->right;
+        }
+        
+        return root;
         
         
         
