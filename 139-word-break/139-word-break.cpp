676c0e569ae970052e025c9a6b4fe0e3aa93d4b3
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-29 10:17:49 +0530

    Time: 31 ms (26.67%), Space: 24.1 MB (5.30%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -6,7 +6,6 @@ public:
         
         int n = s.length();
         if(ind==n) return true;
-        
         if(dp[ind]!=-1) return dp[ind];
         
         
@@ -30,10 +29,13 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         
         int n = s.length();
-        unordered_set<string> st;
+        // unordered_set<string> st;
         
-        for(auto x:wordDict)
-            st.insert(x);
+        // for(auto x:wordDict)
+        //     st.insert(x);
+        //OR
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        
         vector<int> dp(n,-1);
         
         return is_possible(s,0,st,dp);
