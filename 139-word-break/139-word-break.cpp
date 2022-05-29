1faeef2ef9ce75571295174062f78cc8af0003f6
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-05-29 09:50:02 +0530

    Time: 79 ms (5.57%), Space: 24.1 MB (5.41%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool is_possible(string s, int ind, unordered_set<string> &st, vector<int> &dp)
+    {   
+        
+        int n = s.length();
+        if(ind==n) return true;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        
+        
+        string temp = "";
+        for(int i=ind;i<n;i++)
+        {
+            temp = temp + s[i];
+            // cout<<temp<<"\n";
+            if((st.find(temp)!=st.end() && i<n && is_possible(s,i+1,st,dp))) // string is in the set
+            {  
+                if(i<n-1)
+                    dp[i+1] = 1;
+                return true;
+            }                
+        }
+        
+        dp[ind] = 0;
+        return false;
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        int n = s.length();
+        unordered_set<string> st;
+        
+        for(auto x:wordDict)
+            st.insert(x);
+        vector<int> dp(n,-1);
+        
+        return is_possible(s,0,st,dp);
+    }
+};
