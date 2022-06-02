f4c2d2c783836df47a0b57c061d96f67c178535e
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-02 13:50:02 +0530

    Time: 12 ms (53.72%), Space: 9.6 MB (56.03%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,15 +2,32 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        map<int,int> mp;
-        for(auto x:nums)
-            mp[x]++;
+        vector<int> bits(32,0);
+        int res = 0;
+        long long pow = 1;
         
-        for(auto x:mp)
-            if(x.second==1)
-                return x.first;
+        for(int i=0;i<32;i++)
+        {
+            int count = 0;
+            
+            for(auto x:nums)
+                if(((x)&(1<<i)) !=0)
+                    count++;
+            
+            if(count%3 == 1)
+                bits[i] = 1;
+        }
         
         
-        return -1;
+        
+        for(int i=0;i<32;i++)
+        {
+            cout<<bits[i]<<" ";
+            if(bits[i]==1)
+                res = res + (1<<i);
+        }
+            
+        
+        return res;
     }
 };
