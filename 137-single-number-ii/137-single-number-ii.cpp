f976b88c352b3d934d5304d68e6093f35393a257
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-02 13:51:37 +0530

    Time: 21 ms (9.91%), Space: 9.4 MB (98.83%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        vector<int> bits(32,0);
+        vector<int> bits(32);
         int res = 0;
         long long pow = 1;
         
         for(int i=0;i<32;i++)
         {
             int count = 0;
-            
             for(auto x:nums)
                 if(((x)&(1<<i)) !=0)
                     count++;
             
-            if(count%3 == 1)
-                bits[i] = 1;
-        }
-        
-        
-        
-        for(int i=0;i<32;i++)
-        {
-            cout<<bits[i]<<" ";
-            if(bits[i]==1)
-                res = res + (1<<i);
-        }
             
+            if(count%3==1)
+                res += pow;
+            
+            pow = pow*2;
+                
+        }
         
         return res;
     }
