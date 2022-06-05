0f01aa614eba73136a5cc794f8fb75d0c1918fef
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-05 12:50:10 +0530

    Time: 28 ms (10.81%), Space: 7.1 MB (88.51%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -5,38 +5,57 @@ public:
         stack<string> st;
         
         int i=0;
+        reverse(s.begin(), s.end());
         
         while(i<s.length())
         {
             if(s[i]!=' ')
-            {
-                string temp = "";
+            { 
+                int start = i;
+                // string temp = "";
                 while(i<s.length() && s[i]!=' ')
                 {
-                    temp.push_back(s[i]);
+                    // temp = temp + s[i];
+                    // temp.push_back(s[i]);
                     i++;
                 }
                 
+                reverse(s.begin()+start, s.begin()+i);
+                
                 // cout<<temp<<"\n";
                     
-                st.push(temp);
+                // st.push(temp);
             }
             else
-            {
-                i++;
+            {  
+                
+                while(i<s.length() && s[i]==' ')
+                {
+                    if(i>0 && i<s.length()-1 && s[i+1]!=' ')
+                    {
+                        i++;
+                        break;
+                    }
+                    s.erase(s.begin()+i);
+                }
+
+                
+                // i++;
             }  
         }
         
         string res = "";
-        while(!st.empty())
-        {
-            string curr_str =  st.top();
-            st.pop();
-            res = res + curr_str + " ";
-        }
+//         while(!st.empty())
+//         {
+//             string curr_str =  st.top();
+//             st.pop();
+//             res = res + curr_str + " ";
+//         }
         
-        res.pop_back();
+//         res.pop_back();
         
-        return res;
+        // cout<<s;
+        
+        return s;
     }
 };
