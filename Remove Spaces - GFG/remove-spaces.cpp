5f066e6f0ac8f89bfcdde0f24a4b107701f2a6b4
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-05 19:37:46 +0530

    Added solution - LeetHub

--- Remove Spaces - GFG/remove-spaces.cpp
+++ Remove Spaces - GFG/remove-spaces.cpp
@@ -9,23 +9,44 @@ class Solution
   public:
     string modify (string s)
     {
+        int count = 0;
         int i=0;
         string res = "";
         while(i<s.length())
         {
             if(s[i]!=' ')
             {
-                while(i<s.length() && s[i]!=' ')
-                {
-                    // res = res + s[i];
-                    i++;
-                }
+                res = res + s[i];
+                // s[count] = s[i];
+                // count++;
             }
-            else
-            s.erase(s.begin()+i);
+            
+            i++;
+             
         }
         
-        return s;
+        // s[count] = '\0';
+        
+        return res;
+        
+        
+        //ANOTHER APPROACH
+        
+        // int i=0;
+        // string res = "";
+        // while(i<s.length())
+        // {
+        //     if(s[i]!=' ')
+        //     {
+        //         while(i<s.length() && s[i]!=' ')
+        //             i++;
+
+        //     }
+        //     else
+        //     s.erase(s.begin()+i);
+        // }
+        
+        // return s;
     }
 };
 
