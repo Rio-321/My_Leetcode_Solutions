6da0ce20d22781ffa35317e61f155f4b9256f5df
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-06 11:10:19 +0530

    Time: 70 ms (10.54%), Space: 26.4 MB (88.64%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -3,34 +3,37 @@ public:
     int findUnsortedSubarray(vector<int>& nums) {
         
         int n = nums.size();
-        vector<int> temp;
-            for(auto x:nums)
-                temp.push_back(x);
+//         vector<int> temp;
+//             for(auto x:nums)
+//                 temp.push_back(x);
            
-        sort(temp.begin(), temp.end());
+//         sort(temp.begin(), temp.end());
         
         int left = 0;
         int right = -1;
         
-        for(int i=0;i<n;i++)
-        {
-            if(temp[i]!=nums[i])
-            {
-                left = i;
-                break;
-            }
-            
-        }
+        int minA = INT_MAX;
+        int maxA = INT_MIN;
         
         for(int i=n-1;i>=0;i--)
         {
-            if(temp[i]!=nums[i])
-            {
-                right = i;
-                break;
-            }
+            if(minA<nums[i])
+                left = i;
+            
+            minA = min(minA, nums[i]);
         }
-    
+        
+        for(int i=0;i<n;i++)
+        {
+            if(maxA>nums[i])
+                right = i;
+            
+            maxA = max(maxA, nums[i]);
+        }
+        
+        // cout<<left<<" "<<right<<"\n";
+        
+   
         int res = right-left+1;
         
         return res;
