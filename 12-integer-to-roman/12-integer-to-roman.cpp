a2481d23325c1e1dc0be2ba0ac3533f4b54707e1
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-10 12:09:50 +0530

    Time: 11 ms (58.38%), Space: 12 MB (12.94%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    string intToRoman(int n) {
+        
+    int mult = 1;
+
+    string ans = "";
+
+    stack<int> st;
+
+    map<int,char> mp;
+    mp[1] = 'I';
+    mp[5] = 'V';
+    mp[10] = 'X';
+    mp[50] = 'L';
+    mp[100] = 'C';
+    mp[500] = 'D';
+    mp[1000] = 'M';
+    mp[5000] = 'S';
+
+    vector<int> vec = {1,5,10,50,100,500,1000,5000};
+        
+        
+    while(n)
+    {
+        st.push((n%10)*mult);
+        mult = mult*10;
+        n = n/10;
+    }
+
+    mult = mult/10;
+        
+    while(!st.empty())
+    {
+        int p = st.top();
+        st.pop();
+        
+        if(p==0)
+        {
+            mult = mult/10;
+            continue;
+        }
+            
+
+        int l = 0, r = vec.size()-1;
+        int res = 0;
+        while(l<=r)
+        {
+            int mid  = (l+r)/2;
+            if(vec[mid]==p)
+            {
+                res = mid;
+                break;
+            }
+
+            if(vec[mid]>p)
+            {
+                r = mid-1;
+            }
+            else
+            {
+                res = mid;
+                l = mid+1;
+            }
+        }
+        
+
+        if(vec[res+1]-p==mult)
+        {
+            for(int i=0;i<(vec[res+1]-p)/mult;i++)
+                ans.push_back(mp[mult]);
+
+            ans.push_back(mp[vec[res+1]]);
+        }
+        else
+        {
+            ans.push_back(mp[vec[res]]);
+            for(int i=0;i<(p-vec[res])/mult;i++)
+                ans.push_back(mp[mult]);
+        }
+
+        mult = mult/10;
+    }
+        
+        
+        return ans;
+
+    }
+};
