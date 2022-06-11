6991f8b4979197ff0c590843a98255735d1204b6
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-12 00:31:33 +0530

    Time: 46 ms (22.78%), Space: 29.3 MB (7.04%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -1,38 +1,89 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    vector<TreeNode*> nodes;
-
 public:
-    bool isSubtree(TreeNode* s, TreeNode* t) {
-        if (!s && !t) return true;
-        if (!s || !t) return false;
-
-        getDepth(s, getDepth(t, -1));
-
-        for (TreeNode* n: nodes)
-            if (identical(n, t))
-                return true;
-
-        return false;
+    vector<TreeNode*> nodes;
+    
+    bool is_identical(TreeNode* root, TreeNode* subRoot)
+    {
+        if(root==NULL && subRoot==NULL) return true;
+        if(root==NULL || subRoot==NULL) return false;
+        
+        if(root->val != subRoot->val) return false;
+        
+        return (is_identical(root->left, subRoot->left) && is_identical(root->right,subRoot->right));
     }
-
-    int getDepth(TreeNode* r, int d) {
-        if (!r)
-            return -1;
-
-        int depth = max(getDepth(r->left, d), getDepth(r->right, d)) + 1;
-
-        // Check if depth equals required value
-        // Require depth is -1 for tree t (only return the depth, no push)
-        if (depth == d)
-            nodes.push_back(r);
-
-        return depth;
+    
+//     void check_Nodes(TreeNode* root,int d, int curr_d, TreeNode* subRoot)
+//     {
+//         if(root==NULL) return;
+        
+//         if(curr_d==d)
+//         {
+//             if(root->val==subRoot->val)
+//                 nodes.push_back(root);
+            
+//             return;
+//         }
+//         else
+//         {
+//             check_Nodes(root->left,d,curr_d-1,subRoot);
+//             check_Nodes(root->right,d,curr_d-1,subRoot);
+//         }
+            
+        
+//     }
+    
+    int MaxDepth(TreeNode* root, int d)
+    {
+        if(root==NULL) return 0;
+        
+        int dept = max(MaxDepth(root->left,d),MaxDepth(root->right,d)) + 1;
+        
+        
+        if(dept==d)
+            nodes.push_back(root);
+        
+        return dept;
     }
+    
+    
+    
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        
 
-    bool identical(TreeNode* a, TreeNode* b) {
-        if (!a && !b) return true;
-        if (!a || !b || a->val != b->val) return false;
+    int sub_depth = MaxDepth(subRoot, -1);
+    int depth = MaxDepth(root, sub_depth);
+    
+    // cout<<d<<" "<<depth<<"\n";
+        
+    // check_Nodes(root,d,depth,subRoot);
+    // cout<<nodes.size()<<"\n";
+        
+    // cout<<nodes[0]->val<<"\n";
+        
+    // cout<<depth;
 
-        return identical(a->left, b->left) && identical(a->right, b->right);
+    for(auto Node:nodes)
+    {
+        if(is_identical(Node,subRoot))
+            return true;
     }
+        
+    return false;
+ }
 };
+
+
+
+
+// https://leetcode.com/problems/subtree-of-another-tree/discuss/102734/19ms-C%2B%2B-solution-beats-99.9
