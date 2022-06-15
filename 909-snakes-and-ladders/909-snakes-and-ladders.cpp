3cb3e63d45d905bb0643850abac29055b3ae0153
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-15 17:05:52 +0530

    Time: 82 ms (11.63%), Space: 14 MB (30.78%) - LeetHub

---
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -0,0 +1,126 @@
+class Solution {
+public:
+    
+    int No_Of_Moves(int curr, int dest, unordered_map<int,int> path, int n)
+    {  
+        queue<int> q;
+        q.push(1);
+        q.push(0);
+        int ans = 0;
+        
+        vector<bool> visited(n*n+1);
+        visited[1] = true;
+        
+        
+        while(q.size()>1)
+        {
+            int curr = q.front();
+            q.pop();
+            
+            cout<<curr<<" ";
+            if(curr==0)
+            {
+                q.push(0);
+                ans++;
+                cout<<"\n";
+                continue;
+            }
+            
+            if(curr==dest)
+                return ans;
+
+             for(int i=1;i<=6 && curr+i<=n*n;i++)
+            {
+                int next = curr+i;
+                if(path.find(next)!=path.end())
+                {
+                    if(path[next]<next)
+                    {
+                       if(!visited[path[next]])
+                       {
+                           visited[path[next]] = true;
+                           q.push(path[next]);
+                       }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    else if(path[next]>next)
+                    {
+                        if(!visited[path[next]])
+                        {
+                            visited[path[next]] = true;
+                            q.push(path[next]);
+                        }  
+                        else
+                            continue;
+                    }
+
+                }
+                 else
+                {
+                    if(!visited[next])
+                     {
+                         visited[next] = true;                       
+                         q.push(next);
+                     }
+                }
+
+            }
+                 
+             
+                
+        }
+        
+        return -1;
+           
+    }
+        
+
+    
+    
+    int snakesAndLadders(vector<vector<int>>& board) {
+        
+        int n = board.size();
+        
+        unordered_map<int,int> path;
+        
+        int num  = 1;   
+        int flag = 0;
+        for(int i=n-1;i>=0;i--)
+        {    
+            flag = flag ^ 1;
+            if(flag==1)
+            {
+                for(int j=0;j<n;j++)
+                { 
+                    if(board[i][j]!=-1)
+                        path[num] = board[i][j];
+                    // cout<<num<<" ";
+                    num++;
+                }
+                    
+            }
+            else
+            {
+                for(int j=n-1;j>=0;j--)
+                {
+                    if(board[i][j]!=-1)
+                        path[num] = board[i][j];
+                    // cout<<num<<" ";
+                    num++;
+                }
+                    
+            }
+            // cout<<"\n";
+        }
+        
+        
+        for(auto x:path)
+            cout<<x.first<<" - "<<x.second<<"\n";
+        
+        return No_Of_Moves(1,n*n,path,n);
+
+    }
+};
