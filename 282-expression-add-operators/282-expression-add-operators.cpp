3ce22c3dd6d0b77a04ac84e347aa3dcedea42290
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-17 10:26:14 +0530

    Time: 725 ms (26.38%), Space: 115.8 MB (18.03%) - LeetHub

--- 282-expression-add-operators/282-expression-add-operators.cpp
+++ 282-expression-add-operators/282-expression-add-operators.cpp
@@ -2,60 +2,7 @@ class Solution {
 public:
     vector<string> ans;
     
-    void value(string num, int target)
-    {
-        num = num + "+";
-        long long res = 0;
-        long long prev = 0;
-        char sign = '+';
-        long long curr = 0;
-        
-        
-        for(auto c:num)
-        {  
-             if(c==' ') continue;
-          
-            if(isdigit(c))
-            {
-                curr = curr*10 + (c-'0');
-            }
-            else
-            {
-                if(sign=='*')
-                {
-                    prev = prev*curr;
-                    curr=0;
-                    sign = c;
-                }
-                else 
-                {
-                    if(sign=='-')
-                    {
-                       res =  res + prev;
-                        prev = -curr;
-                    } 
-                    else if(sign=='+')
-                    {
-                        res = res + prev;
-                        prev = curr;
-                    }
 
-                    curr = 0;
-                    sign = c;
-                }
-            }
-        }
-        
-        res = res + prev;
-        
-        num.pop_back();
-        
-        
-        // return res;        
-        if(res==target)
-            ans.push_back(num);
-    }
-    
     
     void solve(const string &path, string num, int index, int target, long long resSoFar, long long prevNum)
     {
@@ -108,4 +55,6 @@ public:
         
         return ans;
     }
+    
+    // best solution - https://leetcode.com/problems/expression-add-operators/discuss/572099/C%2B%2BJavaPython-Backtracking-and-Evaluate-on-the-fly-Clean-and-Concise
 };
