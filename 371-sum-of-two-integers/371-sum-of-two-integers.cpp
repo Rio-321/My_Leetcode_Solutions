04e3a7a15a4a79d9a6155842fc1a525ca1c77100
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-17 07:58:41 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (73.60%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,73 +1,18 @@
 class Solution {
 public:
     
-    // MY SOLUTION
+    // ANother Approach
     int getSum(int a, int b) {
-        
-        string res = "";
-        int rem = 0;
-        
-        for(int i=0;i<32;i++)
-        {  
-            if(((a>>i & 1) & (b>>i & 1))==1)
-            {   
-                if(rem==1)
-                {
-                    res.push_back('1');
-                    rem = 1;
-                }
-                else
-                {
-                    res.push_back('0');
-                    rem = 1;
-                }
-            }
-            else if(((a>>i & 1)|(b>>i & 1))==1)
-            {
-                
-                if(rem==1)
-                {
-                    res.push_back('0');
-                    rem = 1;
-                }
-                else
-                {
-                    res.push_back('1');
-                    rem = 0;
-                }
-            }
-            else if(((a>>i & 1)|(b>>i & 1))==0)
-            {
-                if(rem == 1)
-                {
-                    res.push_back('1');
-                    rem = 0;
-                }
-                else
-                {
-                    res.push_back('0');
-                    rem = 0;
-                }
-            }
-            
-
-            
-        }
-        
-        
-        int pow = 1;
-        int ans = 0;
-        
-        for(int i=0;i<res.length();i++)
+          while(b != 0)
         {
-            if(res[i]=='1')
-            {
-                ans = ans + (1<<i);
-            }
+            unsigned int carry = a&b;
+            a = a^b;
+            b = carry<<1;
         }
         
-        return ans;
-        
-        
+        return a;
     }
+    
+    
+    // https://leetcode.com/problems/sum-of-two-integers/discuss/132479/Simple-explanation-on-how-to-arrive-at-the-solution
 };
