e9df1091162eb5e8dccc0fcda2519b48b3124e5e
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-25 19:34:21 +0530

    Time: 41 ms (17.33%), Space: 56.3 MB (6.80%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    string addStrings(string num1, string num2) {
-        
-        string temp;
-        int rem = 0;
-        long long n;
-        
-        int i=num1.length()-1,j=num2.length()-1;
-        
-        while(i>=0 || j>=0 || rem==1)
-        {  
+    string addStrings(string str1, string str2) {
+       string res = "";
 
-            n = 0;
-            if(i>=0)
-            {
-                n = n + (num1[i]-'0');
-                i--;
-            }
-            if(j>=0)
-            {
-                n = n +(num2[j]-'0');
-                j--;
-            }
-            
-            n = n + rem;
-            
-            rem =  n/10;
-            n = n%10;
-            
-            temp += char(n+'0');
-        }
-        
-        
-        reverse(temp.begin(), temp.end());
-        return temp;
-        
-        
+    int rem = 0;
+    int curr = 0;
+    int len1 = str1.length();
+    int len2 = str2.length();
+
+    int max_len = max(len1, len2);
+
+    str1 = string( max_len- len1, '0') + str1;
+    str2 = string( max_len- len2, '0') + str2;
+
+    // cout<<str1<<br;
+    // cout<<str2<<br;
+
+
+    for(int i=max_len-1;i>=0;i--)
+    {
+
+        curr = (str1[i] - '0') + (str2[i]-'0') + rem;
+        rem = curr/10;
+        curr = curr % 10;
+        res = char(curr + '0') + res;
+    }
+
+    // cout<<rem<<br;
+
+    if(rem)
+        res = char(rem + '0') + res;
+
+    // cout<<res<<br;
+
+    return res;
         // https://leetcode.com/problems/add-strings/discuss/90453/C%2B%2B_Accepted_13ms
 
     }
