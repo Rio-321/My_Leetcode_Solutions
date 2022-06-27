782ca5c90f0aec51b917bb69e337e78ef14563a2
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-06-27 08:11:22 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -11,36 +11,30 @@ class Solution{
 public:
 
 // Memoization solution
-
-    int Mchain(int i, int j, int *arr, vector<vector<int>> &dp)
-    {
-        if(dp[i][j] != -1)
-            return dp[i][j];
-            
-        if(j == i+1)
-        {
-            dp[i][j] = 0;
-            return 0;
-        }
-
-        
-        int res = INT_MAX;
-        
-        for(int k=i+1; k<j; k++)
-        {
-            res = min(res, Mchain(i,k,arr, dp) + Mchain(k,j,arr,dp) + arr[i]*arr[k]*arr[j]);
-        }
-        
-        dp[i][j] = res;
-        return res;
-        
-    }
-    
     int matrixMultiplication(int N, int arr[])
     {
         // code here
         vector<vector<int>> dp(N, vector<int>(N, -1));
-        return Mchain(0, N-1, arr,dp);        
+        
+        for(int gap = 1; gap<N; gap++)
+        {
+            for(int i=0; i<N && i+gap<N; i++)
+            {
+                int j = i + gap;
+                if(gap == 1)
+                   dp[i][j] = 0;
+                else
+                {
+                    dp[i][j] = INT_MAX;
+                    for(int k = i+1; k<j; k++)
+                    {
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + arr[i]*arr[k]*arr[j]);
+                    }
+                }
+                
+            }
+        }
+        return dp[0][N-1];        
     }
 };
 
