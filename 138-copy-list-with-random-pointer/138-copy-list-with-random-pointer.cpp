f35ff801e2fdc0f8206cff3b3528543fb22f9741
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-05 11:27:49 +0530

    Time: 6 ms (93.47%), Space: 11.1 MB (87.94%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,48 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        
-        //Basic Approach
-        if(head == NULL)
-           return NULL;
-         
-        Node* curr = head;
-        
-        map<Node*, Node*> mp;
-        
-        while(curr != NULL)
-        {
-            Node* temp = new Node(curr->val);
+        Node *iter = head; 
+          Node *front = head;
 
-            mp[curr] = temp;
-            curr = curr->next;
-        }
-        
-        curr = head;
-        
-        
-        while(curr != NULL)
-        {
-            
-            mp[curr]->next = mp[curr->next];
-            mp[curr]->random = mp[curr->random];
-            
-            curr = curr->next;
-        }
-        
-    
-        
-        return mp[head];
+          // First round: make copy of each node,
+          // and link them together side-by-side in a single list.
+          while (iter != NULL) {
+            front = iter->next;
+
+            Node *copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+
+            iter = front;
+          }
+
+          // Second round: assign random pointers for the copy nodes.
+          iter = head;
+          while (iter != NULL) {
+            if (iter->random != NULL) {
+              iter->next->random = iter->random->next;
+            }
+            iter = iter->next->next;
+          }
+
+          // Third round: restore the original list, and extract the copy list.
+          iter = head;
+          Node *pseudoHead = new Node(0);
+          Node *copy = pseudoHead;
+
+          while (iter != NULL) {
+            front = iter->next->next;
+
+            // extract the copy
+            copy->next = iter->next;
+
+            // restore the original list
+            iter->next = front;
+              
+            copy = copy -> next; 
+            iter = front;
+          }
+
+          return pseudoHead->next;
     }
 };
