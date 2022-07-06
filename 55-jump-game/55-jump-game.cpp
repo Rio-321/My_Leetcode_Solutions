ed1919f91d827747284cfafe9dd2f42bdc715f01
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-06 17:59:13 +0530

    Time: 136 ms (29.03%), Space: 48.4 MB (77.58%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -22,3 +22,48 @@ public:
         
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+        
+//         //MY APPROACH
+//         int n = nums.size();
+        
+//         if(n == 1 ) return true;
+//         if(nums[0] == 0) return false;
+        
+        
+//         int maxi = nums[0];
+//         int p = maxi;
+        
+//         int i=1;
+        
+//         while(i < n)
+//         {
+            
+//             maxi = max(maxi, nums[i]+i);
+            
+//             if(maxi >= n-1)
+//                 return true;
+            
+            
+//             if(i == p)
+//             {
+//                 if(maxi == p) 
+//                         return false;
+                
+//                 p = maxi;
+//             }
+            
+//             i++;
+//         }
+        
+        
+//         return false;
+        
+//     }
+// };
