e9eb705dc93d70be05bb181af71f382f23bcc965
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-07 11:20:11 +0530

    Time: 295 ms (54.59%), Space: 81.3 MB (61.75%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -5,6 +5,7 @@ public:
         
         
         int n = prices.size();
+        if(n == 1) return 0;
         int profit = 0;
        
         
@@ -23,29 +24,30 @@ public:
         int maxi = prices[n-1];
         profit = 0;
         
-        for(int i=n-2;i>=0;i--)
+        int res = (pre_profit[n-2], pre_profit[n-1]);
+        
+        for(int i=n-2;i>0;i--)
         {
             profit = max(profit, maxi-prices[i]);
             post_profit[i] = profit;
             
+            res = max(res, pre_profit[i-1] + profit);
+            
             maxi = max(maxi, prices[i]);
         }
         
-        profit = 0;
+//         profit = 0;
         
-        for(int i=0;i<n-1;i++)
-        {
-           profit = max(profit, pre_profit[i]+post_profit[i+1]); 
-        }
-        profit = max({profit, pre_profit[n-1], post_profit[0]});
-        
-        
-//         for(auto x:post_profit)
-//             cout<<x<<" ";
-        
-//         cout<<"\n";
+//         for(int i=0;i<n-1;i++)
+//         {
+//            profit = max(profit, pre_profit[i]+post_profit[i+1]); 
+//         }
+//         profit = max({profit, pre_profit[n-1], post_profit[0]});
         
         
-        return profit;
+
+        
+        
+        return res;
     }
 };
