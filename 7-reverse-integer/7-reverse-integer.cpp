86e79c15b7a4dde49cad104540c40d319394cd7e
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-10 08:04:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.31%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        if(x == INT_MIN)
+            return 0;
+        
+        int sign  = 1;
+        if(x < 0)
+            sign  = -1;
+        
+        x = abs(x);
+        
+        int res = 0;
+        
+        // cout<<INT_MAX<<"\n";
+        
+        
+        
+        while(x>0)
+        {   
+            if(res > INT_MAX/10)
+                return 0;
+            
+            res = res*10 + x%10;
+            x = x/10;
+        }
+        
+        return sign * res;
+    }
+};
