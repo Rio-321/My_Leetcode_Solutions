03cfa645b62b56ab195db203ebd02a3c03c7700f
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-10 08:07:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (31.31%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -2,30 +2,44 @@ class Solution {
 public:
     int reverse(int x) {
         
-        if(x == INT_MIN)
-            return 0;
         
-        int sign  = 1;
-        if(x < 0)
-            sign  = -1;
+//         // MY SOLUTION
+//         if(x == INT_MIN)
+//             return 0;
         
-        x = abs(x);
+//         int sign  = 1;
+//         if(x < 0)
+//             sign  = -1;
         
-        int res = 0;
+//         x = abs(x);
         
-        // cout<<INT_MAX<<"\n";
+//         int res = 0;
         
         
+//         while(x>0)
+//         {   
+//             if(res > INT_MAX/10)
+//                 return 0;
+            
+//             res = res*10 + x%10;
+//             x = x/10;
+//         }
         
-        while(x>0)
-        {   
-            if(res > INT_MAX/10)
+//         return sign * res;
+//     }
+    
+    // OPTIMAL SOLUTION
+        
+        int y  = 0;
+        while(x)
+        {
+            if(y > INT_MAX/10 || y < INT_MIN/10)
                 return 0;
             
-            res = res*10 + x%10;
+            y = y*10 + x%10;
             x = x/10;
         }
-        
-        return sign * res;
+    
+      return y;
     }
 };
