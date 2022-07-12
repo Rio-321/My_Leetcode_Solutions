b494a30f1a624daada7e1a133d7ae492a35914ab
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-12 18:09:55 +0530

    Time: 18 ms (33.57%), Space: 17.5 MB (19.81%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,40 +12,45 @@
 class Solution {
 public:
    
-    unsigned long long widthOfBinaryTree(TreeNode* root) {
+     long long widthOfBinaryTree(TreeNode* root) {
        
         if(!root)
             return 0;
         
         queue<pair<TreeNode*, long long>> q;
         q.push({root, 0});
-        unsigned long long ans = 1;
+         long long ans = 1;
         
         while(!q.empty())
         {
-            unsigned long long count = q.size();
+             long long count = q.size();
             // int nullCount = 0;
             
-            unsigned long long first = -1;
-            unsigned long long last = -1;
+             long long first = -1;
+             long long last = -1;
             
-            for(unsigned long long i=0;i<count;i++)
+            long long mini = -1;
+            for( long long i=0;i<count;i++)
             {
-                pair<TreeNode*, unsigned long long> curr = q.front();
+                pair<TreeNode*,  long long> curr = q.front();
                 q.pop();
                 
                 if(i == 0)
+                {
+                    mini = curr.second;
                     first = curr.second;
+                }
+                    
                 
                 if(i == count-1)
                     last = curr.second;
                 
                 
                 if(curr.first->left)
-                    q.push({curr.first->left, curr.second*2});
+                    q.push({curr.first->left, (curr.second-mini)*2});
                 
                 if(curr.first->right)
-                    q.push({curr.first->right, curr.second*2 + 1});
+                    q.push({curr.first->right, (curr.second-mini)*2 + 1});
                   
             }
             
