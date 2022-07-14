7ce78c6c217a858848842e2c26bec46d8688a336
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-14 22:48:19 +0530

    Time: 11 ms (99.51%), Space: 19.5 MB (58.28%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,8 +2,50 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int vote = 0, majority = -1;
         
-        return nums[nums.size()/2];
+        for(int i=0;i<n;i++)
+        {
+            if(vote == 0)
+            {
+                vote = 1;
+                majority = nums[i];
+            }
+            else
+            {
+                if(nums[i] == majority)
+                    vote++;
+                else
+                    vote--;
+            }
+            
+            // cout<<vote<<" ";
+        }
+        
+        // cout<<vote<<" "<<majority<<"\n";
+        
+        if(vote == 0)
+            return -1;
+        
+        int count = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == majority)
+                count++;
+        }
+        
+        if(count > n/2)
+            return majority;
+        else
+            return -1;
     }
 };
+
+
+
+
+// Boyer-Moore Voting algorithm is used
+
+// Approach 3 - sort the array and return middle element
