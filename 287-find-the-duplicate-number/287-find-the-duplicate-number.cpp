d14dccfe4980671776b74fc709b851ad89a9ea16
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-14 23:13:43 +0530

    Time: 166 ms (65.56%), Space: 61.2 MB (91.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-     
-          int n = nums.size();
+          // My Solution - 
+            // Time - O(n)
+            // space - O(1)
         
-          for(int i=0;i<n;i++)
-          {
-              while(nums[nums[i]-1]  != nums[i])
-              {
-                  swap(nums[i], nums[nums[i]-1]);
-              }
+//           int n = nums.size();
+        
+//           for(int i=0;i<n;i++)
+//           {
+//               while(nums[nums[i]-1]  != nums[i])
+//               {
+//                   swap(nums[i], nums[nums[i]-1]);
+//               }
               
-              if(nums[i] != i+1 && nums[nums[i]-1] == nums[i])
-                  return nums[i];
-          }
+//               if(nums[i] != i+1 && nums[nums[i]-1] == nums[i])
+//                   return nums[i];
+//           }
         
-        return -1;
+//         return -1;
         
-        
-//         int low = 1;
-//         int high = nums.size()-1;
-        
-//         while(low<=high)
-//         {
-//             int mid = (low+high)/2;
-//             cout<<mid<<"\n";
-            
-//             int cnt = 0;
-            
-//             if(nums[mid] < mid+1)
-//                 high = mid-1;
-//             else
-//                 low = mid+1;
-            
-// //             for(auto x:nums)
-// //                 if(x<=mid)
-// //                     cnt++;
-            
-// //             if(cnt>mid)
-// //                 high = mid-1;
-// //             else
-// //                 low = mid+1;
 
-//         }
+// ************************************binary search solution**************
         
-//         return nums[low];
+        
+        int low = 1;
+        int high = nums.size()-1;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            // cout<<mid<<"\n";
+            
+            int cnt = 0;
+
+            for(auto x:nums)
+                if(x<=mid)
+                    cnt++;
+            
+            if(cnt>mid)
+                high = mid-1;
+            else
+                low = mid+1;
+
+        }
+        
+        return low;
     }
 };
