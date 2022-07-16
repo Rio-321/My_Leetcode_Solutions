82673e606a2c4ecb233188dd5dc909ffa2a8d8f2
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-16 13:06:59 +0530

    Time: 127 ms (43.85%), Space: 13.2 MB (78.12%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -7,33 +7,36 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         
-        
+        int sum = 0;
         
         for(int i=0;i<n-3;i++)
         { 
+            sum = 0;
+            
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
             
-            
+             // sum = nums[i];
             
             for(int j=i+1;j<n-2;j++)
             {
                 if(j > i+1 && nums[j] == nums[j-1])
                     continue;
                 
-            
+                
                 
                 int l = j+1, r = n-1;
                 while(l < r)
                 {
+                    
                     if(l>j+1 && nums[l]==nums[l-1])
                     {
                         l++;
                         continue; 
                     }
                     
-                    long long sum =  nums[i];
-                    sum +=  nums[j];
+                    long long sum =  nums[i] + nums[j];
+                    // sum +=  nums[j];
                     sum +=  nums[l];
                     sum += nums[r];
                     
