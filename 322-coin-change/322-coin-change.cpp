cd5331bf3a262871689e6441e86c95c9b354b560
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-19 10:05:11 +0530

    Time: 178 ms (28.50%), Space: 14.5 MB (59.03%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,44 +1,28 @@
 // Time - O(n*amount)
 // space = O(n*amount)
 
-// Memoization solution
+//  Tabulation solution
 class Solution {
 public:
-    
-    int solve(int i, int amount, vector<int> & coins, vector<vector<int>> &dp)
-    {
-        if(amount == 0)
-            return 0;
-        if(i == -1)
-            return 100000;
-        
-        if(dp[amount][i] != -1)
-            return dp[amount][i];
-        
-        
-        int res = solve(i-1,amount,coins, dp);
-        
-        if(amount >= coins[i])
-            res = min(res, solve(i, amount-coins[i], coins, dp)+1);
-            
-        return dp[amount][i] = res ;
-    }
-    
+
     int coinChange(vector<int>& coins, int amount) {
         
-        if(amount == 0)
-            return 0;
-        
+        vector<int> dp(amount+1, INT_MAX);
         int n = coins.size();
         
-        vector<vector<int>> dp(amount+1, vector<int>(n+1,-1));
+        dp[0] = 0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(coins[j] <= i && dp[i-coins[j]] != INT_MAX)
+                { 
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+            }
+        }
         
-        solve(n-1,amount, coins, dp);
-        
-        if(dp[amount][n-1] > 10000)
-            return -1;
-        else
-            return dp[amount][n-1];
+        return (dp[amount] == INT_MAX ? -1 : dp[amount]);
         
     }
 };
