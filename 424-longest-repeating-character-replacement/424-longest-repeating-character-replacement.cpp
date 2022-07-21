81debfbf518c4c4e6814a2b192e768551baf6e75
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-21 09:07:12 +0530

    Time: 18 ms (55.96%), Space: 7.1 MB (52.71%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -5,20 +5,20 @@ public:
         int n = s.size();
         int l = 0,  r = 0;
         
-        vector<int> count(128,0);
+        vector<int> count(27,0);
         int ans = 0;
         int maj = 0;
         
         while(r < n)
         {
-            count[s[r]]++;
-            maj = max(maj, count[s[r]]);
+            count[s[r]-'A']++;
+            maj = max(maj, count[s[r]-'A']);
             
             
             while(maj < r-l+1-k)
             {
                 
-                count[s[l]]--;
+                count[s[l]-'A']--;
                 
                 maj = *max_element(count.begin(), count.end());
 
