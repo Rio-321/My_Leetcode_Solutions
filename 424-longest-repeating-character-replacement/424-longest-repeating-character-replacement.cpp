15043b887fc604cbb7b284ba5bf3ec24dbd77646
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-21 09:16:46 +0530

    Time: 19 ms (53.01%), Space: 6.9 MB (85.87%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,7 @@
+// Time - The time complexity of this solution is O(n) because each element is visited at most twice. In the worst case scenario, all elements will be visited once by the start pointer and another time by the end pointer.
+
+// Space - The space complexity would be O(1) because the solution doesn't create new data structures.
+    
 class Solution {
 public:
     int characterReplacement(string s, int k) {
