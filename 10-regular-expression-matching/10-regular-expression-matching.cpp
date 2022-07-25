e210dbe3f91785cb1ef9ab6178eea0ff3b4cc138
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-25 19:10:51 +0530

    Time: 9 ms (56.00%), Space: 7.3 MB (31.72%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,51 +1,50 @@
-// The time complexity of this brute force approach is O(3^(max(m,n)) and space complexity is O(max(m,n)) where m and n are the length of pattern and string respectively.
+// Memoization Solution - 
+
+// The time complexity is now O(mn) and space complexity is O(mn) where m and n are the length of pattern and string respectively.
 
 class Solution {
-public:
-    
-    vector<vector<int>> dp;
-    bool helper(string s, string p, int i, int j)
-    {
-        if(j == p.size())
-            if(i == s.size())
-                return true;
-            else 
-                return false;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        
-        bool first = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
-        
-        if(j+1 < p.size() && p[j+1] == '*')
-        {
-                if(helper(s, p, i, j+2))
-                    dp[i][j] =  true;
-                else
-                {
-                    if(first == true)
-                        dp[i][j] =   helper(s,p,i+1,j);
-                    else
-                        dp[i][j] =  false;
-                }
-        }
-        else
-        {
-            if(first == true)
-               dp[i][j] =  helper(s, p, i+1, j+1);
-            else
-                dp[i][j] =  false;
-        }
-        
-        return dp[i][j];
-        
-    }
-    
+public:    
     
     bool isMatch(string s, string p) {
         
-        dp = vector<vector<int>>(s.size()+1, vector<int>(p.size()+1, -1));
-        return helper(s,p,0,0);
+        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, 0));
+          
+        dp[s.size()][p.size()] = 1;
+        
+        for(int i=0;i<s.size();i++)
+            dp[i][p.size()] = 0;
+        
+        for(int i=s.size();i>=0;i--)
+        {
+            for(int j=p.size()-1;j>=0;j--)
+            {
+                
+                    bool first = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
+
+                    if(j+1 < p.size() && p[j+1] == '*')
+                    {
+                            if(dp[i][j+2])
+                                dp[i][j] =  1;
+                            else
+                            {
+                                if(first == true)
+                                    dp[i][j] =  dp[i+1][j];
+                                else
+                                    dp[i][j] =  false;
+                            }
+                    }
+                    else
+                    {
+                        if(first == true)
+                           dp[i][j] = dp[i+1][j+1];
+                        else
+                            dp[i][j] =  false;
+                    }
+
+                
+            }
+        }
+        
+        return dp[0][0];
     }
 };
