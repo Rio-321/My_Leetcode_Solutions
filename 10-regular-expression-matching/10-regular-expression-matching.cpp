3cfb73f980865c08fb97eacd07db15043f09fd94
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-25 19:00:07 +0530

    Time: 32 ms (29.93%), Space: 21.2 MB (7.59%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,7 +1,51 @@
+// The time complexity of this brute force approach is O(3^(max(m,n)) and space complexity is O(max(m,n)) where m and n are the length of pattern and string respectively.
+
 class Solution {
 public:
-    bool isMatch(string s, string p) {
     
-        return regex_match(s, regex(p));
+    vector<vector<int>> dp;
+    bool helper(string s, string p, int i, int j)
+    {
+        if(j == p.size())
+            if(i == s.size())
+                return true;
+            else 
+                return false;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        
+        bool first = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
+        
+        if(j+1 < p.size() && p[j+1] == '*')
+        {
+                if(helper(s, p, i, j+2))
+                    dp[i][j] =  true;
+                else
+                {
+                    if(first == true)
+                        dp[i][j] =   helper(s,p,i+1,j);
+                    else
+                        dp[i][j] =  false;
+                }
+        }
+        else
+        {
+            if(first == true)
+               dp[i][j] =  helper(s, p, i+1, j+1);
+            else
+                dp[i][j] =  false;
+        }
+        
+        return dp[i][j];
+        
+    }
+    
+    
+    bool isMatch(string s, string p) {
+        
+        dp = vector<vector<int>>(s.size()+1, vector<int>(p.size()+1, -1));
+        return helper(s,p,0,0);
     }
 };
