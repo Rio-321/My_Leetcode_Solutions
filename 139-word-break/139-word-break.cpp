2db27f28535a42657815d63ec71de76ea8faeb47
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-26 10:26:44 +0530

    Time: 3 ms (95.97%), Space: 11.5 MB (53.40%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,38 +1,89 @@
+class TrieNode{
+public:
+    TrieNode* child[26];
+    bool isend;
+    
+    TrieNode()
+    {
+        isend = false;
+        for(int i=0;i<26;i++)
+            child[i] = NULL;
+    }
+    
+};
+
 class Solution {
 public:
     
-    bool is_possible(string s, int ind, unordered_set<string> &st, vector<int> &dp)
-    {   
-        
-        int n = s.length();
-        if(ind==n) return true;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        
-        
-        string temp = "";
-        for(int i=ind;i<n;i++)
+    TrieNode* root;
+    
+    void insert(string str)
+    {
+        TrieNode* curr = root;
+        int n = str.size();
+        for(int i=0;i<n;i++)
         {
-            temp = temp + s[i];
-
-            if(st.find(temp)!=st.end()) // string is in the set
-            {  
-                if(is_possible(s,i+1,st,dp))
-                    return dp[ind] = true;
-            }                
+            int num = str[i] - 'a';
+            
+            if(curr->child[num] == NULL)
+            {
+                TrieNode* temp = new TrieNode();
+                curr->child[num] = temp; 
+            }
+            
+            curr = curr->child[num];
         }
         
-        return dp[ind] = false;
+        curr->isend = true;
+    }
+    
+
+    bool is_possible(int ind, string str, vector<int> &dp)
+    {   
+        
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+        int n = str.length();
+        
+        TrieNode* curr = root;
+        
+        for(int i=ind;i<n;i++)
+        {
+            if(curr->isend == true)
+            {
+                if(is_possible(i, str,dp))
+                    return dp[ind] = true;
+            }
+            
+            int num = str[i]-'a';
+            
+            if(curr->child[num] == NULL)
+                return dp[ind] = false;
+            else
+            {
+                curr = curr->child[num];
+            }
+             
+        }
+        
+        if(curr->isend == true)
+            return dp[ind] = true;
+        else
+            return dp[ind] = false;
     }
     
     bool wordBreak(string s, vector<string>& wordDict) {
         
+        root = new TrieNode();
+        
+        for(auto str : wordDict)
+            insert(str);
+        
+        
         int n = s.length();
+        vector<int> dp(n, -1);
       
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
-        
-        vector<int> dp(n,-1);
-        
-        return is_possible(s,0,st,dp);
+        return is_possible(0,s, dp);
     }
 };
