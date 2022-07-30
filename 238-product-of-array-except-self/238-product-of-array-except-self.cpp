5ef6dc63b6ce70b3aad1155dd15ca0335938274e
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-30 10:24:56 +0530

    Time: 70 ms (5.32%), Space: 24 MB (83.35%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -13,15 +13,18 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            ans[i] = pre;
+            ans[i] = ans[i] * pre;
             pre = pre * nums[i];
+            
+            ans[n-1-i] = ans[n-1-i]*post;
+            post = post * nums[n-1-i];
         }
         
-        for(int i=n-1;i>=0;i--)
-        {
-            ans[i] = ans[i]*post;
-            post = post * nums[i];
-        }
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             ans[i] = ans[i]*post;
+//             post = post * nums[i];
+//         }
         
         return ans;
         
