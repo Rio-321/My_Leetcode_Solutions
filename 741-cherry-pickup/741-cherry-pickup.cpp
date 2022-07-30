dd32f6bc1b976944480520fff2c1885099e0b6d9
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-30 20:33:33 +0530

    Time: 76 ms (73.19%), Space: 22.4 MB (68.37%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -11,11 +11,13 @@ public:
     {
         int c2 = r1 + c1 - r2;
         
+         if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
+            return INT_MIN;
+        
         if(dp[r1][c1][r2] != -1)
             return dp[r1][c1][r2];
         
-        if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
-            return INT_MIN;
+       
 
         if(r1 == n-1 && c1 == n-1)
             return grid[r1][c1];
@@ -55,7 +57,7 @@ int cherryPickup(vector<vector<int>> &grid)
        
         int n = grid.size();
         
-        dp = vector<vector<vector<int>>> (n+1, vector<vector<int>>(n+1, vector<int>(n+1, -1)));
+        dp = vector<vector<vector<int>>> (n, vector<vector<int>>(n, vector<int>(n, -1)));
         
             
         int ans =  max(0, helper(0,0,0,grid,n));
