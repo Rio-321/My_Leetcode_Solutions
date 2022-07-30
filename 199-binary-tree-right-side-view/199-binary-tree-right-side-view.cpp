e2ff45b567b03b51630b4db95d49ace5ce1cff99
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-07-30 11:59:43 +0530

    Time: 11 ms (17.47%), Space: 12 MB (66.53%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -9,34 +9,72 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     
-    // My Solution
-    vector<int> rightSideView(TreeNode* root) {
+    int max_level;
+    vector<int> ans;
+    
+    void func(TreeNode* root, int level)
+    {
+        if(root==NULL)
+            return;
         
-        if(root==NULL) return {};
-        vector<int> nodes;
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(!q.empty())
+        if(level == max_level)
         {
-            int size = q.size();
-            for(int i=0;i<size;i++)
-            {
-                TreeNode* curr = q.front();
-                q.pop();
-                if(i==size-1)
-                    nodes.push_back(curr->val);
-                
-                if(curr->left!=NULL)
-                    q.push(curr->left);
-                if(curr->right!=NULL)
-                    q.push(curr->right);
-            }
+            ans.push_back(root->val);
+            max_level++;
         }
         
-        return nodes;
+        func(root->right, level+1);
+        func(root->left, level+1);
+        
+        return;
+        
     }
+    
+    vector<int> rightSideView(TreeNode* root)
+    {
+        max_level = 0;
+        
+        func(root, 0);
+        
+        return ans;
+        
+    }
+    
 };
+
+
+// class Solution {
+// public:
+    
+//     // My Solution
+//     vector<int> rightSideView(TreeNode* root) {
+        
+//         if(root==NULL) return {};
+//         vector<int> nodes;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty())
+//         {
+//             int size = q.size();
+//             for(int i=0;i<size;i++)
+//             {
+//                 TreeNode* curr = q.front();
+//                 q.pop();
+//                 if(i==size-1)
+//                     nodes.push_back(curr->val);
+                
+//                 if(curr->left!=NULL)
+//                     q.push(curr->left);
+//                 if(curr->right!=NULL)
+//                     q.push(curr->right);
+//             }
+//         }
+        
+//         return nodes;
+//     }
+// };
