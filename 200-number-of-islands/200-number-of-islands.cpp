99b2785334ca568c56863d01e51ae7d80da52950
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-08 17:13:10 +0530

    Time: 63 ms (39.36%), Space: 17.9 MB (18.78%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -7,62 +7,64 @@ public:
     
     int n,m;
 
-bool isSafe(int x, int y)
-{
-    if(x>=0 && x<m && y>=0 && y<n)
-        return true;
+// bool isSafe(int x, int y)
+// {
+//     if(x>=0 && x<m && y>=0 && y<n)
+//         return true;
     
-    return false;
-}
+//     return false;
+// }
 
     
     void BFS(int i, int j, vector<vector<char>> &grid)
 {
         
-        grid[i][j] = '2';
-        
-        int move[5] = {-1,0,1,0,-1};
-        for(int k=0;k<4;k++)
-        {
-            int Newx = i + move[k];
-            int Newy = j + move[k+1];
-            
-            if(isSafe(Newx,Newy) && grid[Newx][Newy] == '1')
-            {
-                BFS(Newx, Newy, grid);
-            }
-            
-        }
-        
-        
-//     queue<int> q;
-//     q.push(n*i+j);
-    
-//     while(!q.empty())
-//     {
-//         int curr = q.front();
-//         int x = curr/n;
-//         int y = curr%n;
-        
-//         q.pop();
-        
-//         grid[x][y] = '2';
+//         grid[i][j] = '2';
         
 //         int move[5] = {-1,0,1,0,-1};
 //         for(int k=0;k<4;k++)
 //         {
-//             int Newx = x + move[k];
-//             int Newy = y + move[k+1];
+//             int Newx = i + move[k];
+//             int Newy = j + move[k+1];
             
 //             if(isSafe(Newx,Newy) && grid[Newx][Newy] == '1')
 //             {
-//                 q.push(n*Newx + Newy);
+//                 BFS(Newx, Newy, grid);
 //             }
             
 //         }
         
+        
+    queue<int> q;
+    q.push(n*i+j);
+    grid[i][j] = '2';
+    
+    while(!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+        
+        int x = curr/n;
+        int y = curr%n;
+        
+        
+        
+        int move[5] = {-1,0,1,0,-1};
+        for(int k=0;k<4;k++)
+        {
+            int Newx = x + move[k];
+            int Newy = y + move[k+1];
+            
+            if((Newx>=0 && Newx<m && Newy>=0 && Newy<n) && grid[Newx][Newy] == '1')
+            {
+                grid[Newx][Newy] = '2';
+                q.push(n*Newx + Newy);
+            }
+            
+        }
+        
 
-//     }
+    }
 }
 
     
