fba600b16b7186f79da5fa3cbe6304e55351bb90
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-08 17:13:51 +0530

    Time: 89 ms (12.25%), Space: 18 MB (17.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -7,33 +7,17 @@ public:
     
     int n,m;
 
-// bool isSafe(int x, int y)
-// {
-//     if(x>=0 && x<m && y>=0 && y<n)
-//         return true;
+bool isSafe(int x, int y)
+{
+    if(x>=0 && x<m && y>=0 && y<n)
+        return true;
     
-//     return false;
-// }
+    return false;
+}
 
     
     void BFS(int i, int j, vector<vector<char>> &grid)
 {
-        
-//         grid[i][j] = '2';
-        
-//         int move[5] = {-1,0,1,0,-1};
-//         for(int k=0;k<4;k++)
-//         {
-//             int Newx = i + move[k];
-//             int Newy = j + move[k+1];
-            
-//             if(isSafe(Newx,Newy) && grid[Newx][Newy] == '1')
-//             {
-//                 BFS(Newx, Newy, grid);
-//             }
-            
-//         }
-        
         
     queue<int> q;
     q.push(n*i+j);
@@ -55,7 +39,7 @@ public:
             int Newx = x + move[k];
             int Newy = y + move[k+1];
             
-            if((Newx>=0 && Newx<m && Newy>=0 && Newy<n) && grid[Newx][Newy] == '1')
+            if(isSafe(Newx, Newy) && grid[Newx][Newy] == '1')
             {
                 grid[Newx][Newy] = '2';
                 q.push(n*Newx + Newy);
