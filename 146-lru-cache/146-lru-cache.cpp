bd9fdd70dfd39f2fa6a28a62307b2ca5a6e256bf
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-12 07:48:06 +0530

    Time: 571 ms (72.43%), Space: 163.2 MB (98.82%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,86 +1,105 @@
 class LRUCache {
 public:
-    class node{
-        
+
+    class Node{
         public:
+        
         int key;
-        int val;
-        node* next;
-        node* prev;
-        node(int _key, int _val)
+        int value;
+        Node* prev;
+        Node* next;
+        
+        
+        Node(int key, int value)
         {
-            key = _key;
-            val = _val;
+            this->value = value;
+            this->key = key;
+            prev = NULL;
+            next = NULL;
         }
+            
     };
     
-    node* head = new node(-1,-1);
-    node* tail = new node(-1,-1);
-    int cap;
-    unordered_map<int, node*> mp;
+    Node* head = new Node(-1,-1);
+    Node* tail = new Node(-1,-1);
     
-
-    LRUCache(int capacity) {
-        
+    unordered_map<int, Node*> mp;
+    int cap;
+   
+    LRUCache(int capacity) 
+    {
         cap = capacity;
         head->next = tail;
-        tail->prev = head;  
+        tail->prev = head;
     }
+ 
     
-    void addnode(node* newnode)
+    int get(int key) 
     {
-        node* temp = head->next;
-        newnode->next = temp;
-        newnode->prev = head;
-        head->next = newnode;
-        temp->prev = newnode;
-    }
-    
-    void deletenode(node* delnode)
-    {
-        node* delprev = delnode->prev;
-        node* delnext = delnode->next;
-        delprev->next = delnext;
-        delnext->prev = delprev;
-    }
-    
-    int get(int key_) 
-    {
-        
-        if(mp.find(key_) != mp.end())
-        {
-            node* resnode = mp[key_];
-            int res = resnode->val;
-            mp.erase(key_);
-            deletenode(resnode);
-            addnode(resnode);
-            mp[key_] = head->next;
-            return res;
-        }
+         if(mp.find(key) != mp.end()) // key is present
+         {
+             Node* resnode = mp[key];
+             int res = resnode->value;
+             
+             resnode->prev->next = resnode->next;
+             resnode->next->prev = resnode->prev;
+             
+             resnode->next = head->next;
+             head->next = resnode;
+             resnode->next->prev = resnode;
+             resnode->prev = head;
+             
+             return res;
+         }
         
         return -1;
-        
-     
     }
     
-    void put(int key_, int value) {
-        
-        if(mp.find(key_) != mp.end())
+    void put(int key, int value) 
+    {
+             if(mp.find(key) != mp.end())
+             {
+                 Node* resnode = mp[key];
+                 resnode->value = value;
+                 
+                 resnode->prev->next = resnode->next;
+                 resnode->next->prev = resnode->prev;
+
+                 resnode->next = head->next;
+                 head->next = resnode;
+                 resnode->next->prev = resnode;
+                 resnode->prev = head;
+                   
+             }
+        else
         {
-            node* existingnode = mp[key_];
-            mp.erase(key_);
-            deletenode(existingnode);
+            if(mp.size() == cap)
+            {
+                Node* resnode = tail->prev;
+                mp.erase(resnode->key);
+                resnode->key = key;
+                resnode->value = value;
+                mp[key] = resnode;
+                
+                 resnode->prev->next = resnode->next;
+                 resnode->next->prev = resnode->prev;
+
+                 resnode->next = head->next;
+                 head->next = resnode;
+                 resnode->next->prev = resnode;
+                 resnode->prev = head;
+            }
+            else
+            {
+                 Node* temp = new Node(key, value);
+                 mp[key] = temp;
+                 
+                 temp->next = head->next;
+                 head->next = temp;
+                 temp->next->prev = temp;
+                 temp->prev = head;   
+            }
         }
-        
-        if(mp.size() == cap)
-        {
-            mp.erase(tail->prev->key);
-            deletenode(tail->prev);
-        }
-        
-        addnode(new node(key_, value));
-        mp[key_] = head->next;
-         
     }
 };
 
