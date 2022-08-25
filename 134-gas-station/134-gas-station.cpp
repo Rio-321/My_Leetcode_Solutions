e69582260b5d2c821f962e78eac4db3d006c8536
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-25 08:59:05 +0530

    Time: 114 ms (68.22%), Space: 69.4 MB (53.18%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,54 +1,32 @@
+// https://leetcode.com/problems/gas-station/discuss/42568/Share-some-of-my-ideas.
+
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         
-        int n = gas.size();
-        int tot_gas = 0;
-        for(int i=0;i<n;i++)
-            tot_gas+=(gas[i]-cost[i]);
+       int n = gas.size();
+       int tank = 0;
+       int total = 0;
+        int start = 0;
+        
+       for(int i=0;i<n;i++)
+       {
+           tank += (gas[i] - cost[i]);
+           total += (gas[i] - cost[i]);
+           
+           if(tank < 0)
+           {
+               start = i+1;
+               tank = 0;
+           }
+       }
+        
+      if(total < 0)
+          return -1; 
+        else
+            return start;
         
         
-        if(tot_gas<0)
-            return -1;
-        
-        
-            for(int i=0;i<n;i++)
-            {   
-                int ind = i;
-                
-                if(gas[i]>=cost[i])
-                {   
-                    // cout<<i<<" ";
-                    long long int rem_fuel  = gas[i]-cost[i];
-                    bool flag = true;
-                    for(int j=(i+1)%n; j!=i; j=(j+1)%n)
-                    {
-                        rem_fuel = rem_fuel + (gas[j]-cost[j]);
-                        
-                        if(rem_fuel<0)
-                        {  
-                            flag = false;
-                            i = j;
-                            break;
-                            
-                        }
-                        // else
-                            // rem_fuel = rem_fuel - cost[j];
-                            
-                    }
-                    
-                    if(flag==true)
-                        return i;
-                    
-                    if(i+1<=ind)
-                        break;
-                    
-                }
-                
-                
-            }
-            
-            return -1;
 
     }
 };
