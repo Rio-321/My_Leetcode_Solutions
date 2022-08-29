cdc8d5bb6d347403cf7d4fdf530e8c9c668f7682
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-29 16:05:36 +0530

    Time: 3 ms (98.99%), Space: 7.6 MB (85.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,36 +1,85 @@
 class Solution {
 public:
     // https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
+    
+    
     int lengthOfLongestSubstring(string s) {
         
-        if(s.length() < 2)
-            return s.length();
+        if(s.size() < 2)
+              return s.size();
+          
+        vector<int> freq(128, 0);
         
-        int res = INT_MIN;
-        int left = 0, right = 0;
+        int left = 0, end = 0;
         int count = 0;
+        int ans = 0;
         
-        int map[128] = {0};
-        
-        while(right < s.length())
+        while(end < s.size())
         {
-            if(map[s[right]]++ >= 1)
+            freq[s[end]]++;
+            
+            if(freq[s[end]] > 1)
                 count++;
             
-            while(count > 0)
-            {
-                if(map[s[left]]-- > 1)
-                    count--;
-                
-                left++;
-            }
+             while(count > 0)
+             {
+                 freq[s[left]]--;
+                 
+                 if(freq[s[left]] >= 1)
+                     count--;
+                 
+                 left++;
+             }
             
-            res = max(res, right - left + 1);
-            
-            
-            right++;
+             ans = max(ans, end-left+1);
+             end++;
         }
         
-        return res;
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         if(s.length() < 2)
+//             return s.length();
+        
+//         int res = INT_MIN;
+//         int left = 0, right = 0;
+//         int count = 0;
+        
+//         int map[128] = {0};
+        
+//         while(right < s.length())
+//         {
+//             if(map[s[right]]++ >= 1)
+//                 count++;
+            
+//             while(count > 0)
+//             {
+//                 if(map[s[left]]-- > 1)
+//                     count--;
+                
+//                 left++;
+//             }
+            
+//             res = max(res, right - left + 1);
+            
+            
+//             right++;
+//         }
+        
+//         return res;
     }
 };
