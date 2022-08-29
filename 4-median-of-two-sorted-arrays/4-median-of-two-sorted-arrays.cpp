297a2e4dca15c8486478a9c75f668bcbe9bb6b65
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-29 14:17:31 +0530

    Time: 47 ms (81.97%), Space: 89.4 MB (62.58%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -5,92 +5,46 @@ public:
     
     // https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2193858/Easy-C%2B%2B-Sol-oror-Naive-greaterEfficient-Approaches-oror-Binary-Search
     
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        
-        int len = (n1 + n2);
-        
-        if(len == 1)
-        {
-            if(n1 == 1)
-                return nums1[0];
-            else
-                return nums2[0];
-        }
-        
-        int ind1 = (len - 1)/2;
-        int ind2 = (len + 1)/2;
-        
-        // cout<<ind2<<"\n";
+       if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2, nums1);
     
-        
-        int val1 = 0, val2 = 0;
-        
-        int i = 0;
-        int one = 0, two = 0;
-        while(one < n1 && two < n2 && i<=ind2)
-        {
-            if( nums1[one] > nums2[two])
-            {
-                if(i == ind1)
-                    val1 = nums2[two];
-                
-                if(i == ind2)
-                    val2 = nums2[two];
-                
-                two++;
-                i++;
-                
-            }
-            else
-            { 
-                if(i == ind1)
-                    val1 = nums1[one];
-                
-                if(i == ind2)
-                    val2  = nums1[one];
-                
-                one++;
-                i++;
-            }
-            
-            
-        }
-        
-        while( one < n1 && i <= ind2)
-        {
-             if(i == ind1)
-                    val1 = nums1[one];
-                
-            if(i == ind2)
-                    val2 = nums1[one];
-                
-              one++;
-              i++;
-        }
-        
-        while(two < n2 && i <= ind2)
-        {
-            if(i == ind1)
-                    val1 = nums2[two];
-                
-            if(i == ind2)
-                   val2  = nums2[two];
-                
-                two++;
-                i++;
-        }
-        
-        // cout<<val1<< " "<< val2;
-        
-        if(len % 2 == 1)
-        {
-            return (double)val1;
-        }
-        else
-            return (double)(val1 + val2)/2.00;
+       int n1 = nums1.size();
+       int n2 = nums2.size();
+    
+       int low = 0, high = n1;
+    
+       while(low <= high)
+       {
+           int cut1 = low + (high-low)/2;
+           int cut2 = (n1+n2+1)/2 - cut1;
+           
+           
+           int left1 = (cut1 == 0) ? INT_MIN: nums1[cut1-1];
+           int left2 = (cut2 == 0) ? INT_MIN : nums2[cut2-1];
+           
+           int right1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+           int right2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+           
+           
+           if(left1 <= right2 && left2 <= right1)
+           {
+               if((n1+n2)%2 == 0)
+               {
+                   return (max(left1, left2) + min(right1, right2))/2.0;
+               }
+               else
+                    return max(left1, left2);
+           }
+           
+           if(left1 > right2)
+               high = cut1-1;
+           else
+               low = cut1+1;
+           
+       }
+    
+        return 0.0;
             
     }
 };
