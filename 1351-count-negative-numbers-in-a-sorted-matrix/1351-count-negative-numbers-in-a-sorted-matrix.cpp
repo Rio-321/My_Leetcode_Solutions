ee5618ca065603397a4ec158b8c0a4576f9157e2
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-08-31 13:39:09 +0530

    Time: 37 ms (10.77%), Space: 10.4 MB (47.64%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,38 +1,59 @@
 class Solution {
 public:
     
-    int f(vector<vector<int>> &grid, int i, int low, int high)
-    {       
-            int n = high+1;
-            int ans = n;
-            while(low<=high)
-            {
-                int mid = (low+high)/2;
-                if(grid[i][mid]<0 && (mid==0 || grid[i][mid-1]>=0))
-                    return n-mid;
+//     int f(vector<vector<int>> &grid, int i, int low, int high)
+//     {       
+//             int n = high+1;
+//             int ans = n;
+//             while(low<=high)
+//             {
+//                 int mid = (low+high)/2;
+//                 if(grid[i][mid]<0 && (mid==0 || grid[i][mid-1]>=0))
+//                     return n-mid;
                 
-                if(grid[i][mid]>=0)
-                    low = mid+1;
-                else
-                {
-                    ans = mid;
-                    high = mid-1;
-                }
-            }
+//                 if(grid[i][mid]>=0)
+//                     low = mid+1;
+//                 else
+//                 {
+//                     ans = mid;
+//                     high = mid-1;
+//                 }
+//             }
             
-            return n-ans;
-    }
+//             return n-ans;
+//     }
     
-    int countNegatives(vector<vector<int>>& grid) {
+    int countNegatives(vector<vector<int>>& mat) {
         
-        int m = grid.size();
-        int n = grid[0].size();
-        int res = 0;
-        for(int i=0;i<m;i++)
-        {
-            res = res + f(grid,i,0,n-1);  
+        int m = mat.size();
+        int n = mat[0].size();
+//         int res = 0;
+//         for(int i=0;i<m;i++)
+//         {
+//             res = res + f(grid,i,0,n-1);  
+//         }
+        
+         
+          int i = m-1, j = 0;
+          int ans = 0;
+        
+        while(i>=0)
+        {  
+            
+            while(j<n && mat[i][j]>=0)            
+                    j++;
+            
+            ans += (n-j);
+            
+            i--;
+            // j++;
+            
+            
         }
+
+        return ans;
         
-        return res;
+        
+        
     }
 };
