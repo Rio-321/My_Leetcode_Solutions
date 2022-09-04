da8ae4b33d130b64a8ba55064bd01551602ef7b7
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-09-04 10:27:07 +0530

    Time: 33 ms (58.35%), Space: 13 MB (44.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int longestCommonSubsequence(string str1, string str2) {
-        int res = 0;
-        int m = str1.length();
-        int n = str2.length();
-    
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        for(int i=0;i<=m;i++)
-            dp[i][0] = 0;
-        for(int i=0;i<=n;i++)
-            dp[0][i] = 0;
         
+        int m = str1.size();
+        int n = str2.size();
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
         
         for(int i=1;i<=m;i++)
         {
             for(int j=1;j<=n;j++)
             {
-                if(str1[i-1]==str2[j-1])
-                    dp[i][j] = 1+dp[i-1][j-1];
+                if(str1[i-1] == str2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
@@ -25,6 +20,32 @@ public:
         
         
         return dp[m][n];
+        
+//         int res = 0;
+//         int m = str1.length();
+//         int n = str2.length();
+    
+//         vector<vector<int>> dp(m+1,vector<int>(n+1));
+        
+//         for(int i=0;i<=m;i++)
+//             dp[i][0] = 0;
+//         for(int i=0;i<=n;i++)
+//             dp[0][i] = 0;
+        
+        
+//         for(int i=1;i<=m;i++)
+//         {
+//             for(int j=1;j<=n;j++)
+//             {
+//                 if(str1[i-1]==str2[j-1])
+//                     dp[i][j] = 1+dp[i-1][j-1];
+//                 else
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//             }
+//         }
+        
+        
+//         return dp[m][n];
     
     }
 };
