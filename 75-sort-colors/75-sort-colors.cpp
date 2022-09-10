1d7f6bce03af042b7c910424e1000be23c0d0c97
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-09-10 12:59:59 +0530

    Time: 3 ms (64.01%), Space: 8.3 MB (70.17%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
+        int n = nums.size();
+        int low = 0, high = n-1;
+        int mid = 0;
+        
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+        
         
         //Optimal Approach
         // [0,low-1] --> 0
@@ -9,29 +32,29 @@ public:
         // [high+1, n-1] --> 2
         // mid --> current index
         
-        int low = 0, high = nums.size()-1;
-        int mid = 0;
+//         int low = 0, high = nums.size()-1;
+//         int mid = 0;
         
-       while(mid <= high)
-       {
-           switch (nums[mid]){
+//        while(mid <= high)
+//        {
+//            switch (nums[mid]){
                    
-               case 1:
-                   mid++;
-                   break;
-               case 2:
-                   swap(nums[mid], nums[high]);
-                   high--;
-                   break;
+//                case 1:
+//                    mid++;
+//                    break;
+//                case 2:
+//                    swap(nums[mid], nums[high]);
+//                    high--;
+//                    break;
                         
-               case 0:
-                    swap(nums[mid], nums[low]);
-                    low++;
-                    mid++;
-                    break;
+//                case 0:
+//                     swap(nums[mid], nums[low]);
+//                     low++;
+//                     mid++;
+//                     break;
                     
-           }
-       }
+//            }
+//        }
         
         
     }
