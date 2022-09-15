bf14269cc798cc999afc48e1b2332d33e551bb05
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-09-15 23:21:47 +0530

    Time: 39 ms (43.59%), Space: 12.2 MB (99.89%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.cpp
+++ 909-snakes-and-ladders/909-snakes-and-ladders.cpp
@@ -1,113 +1,64 @@
+// https://leetcode.com/problems/snakes-and-ladders/discuss/173378/Diagram-and-BFS
+// see comment from shenglia
+
 class Solution {
 public:
-    
-    // MY Solution
-    int No_Of_Moves(int curr, int dest, unordered_map<int,int> path, int n)
-    {  
-        queue<int> q;
-        q.push(1);
-        q.push(0);
-        int ans = 0;
-        
-        vector<bool> visited(n*n+1);
-        visited[1] = true;
-        
-        
-        while(q.size()>1)
-        {
-            int curr = q.front();
-            q.pop();
-            
-            // cout<<curr<<" ";
-            if(curr==0)
-            {
-                q.push(0);
-                ans++;
-                // cout<<"\n";
-                continue;
-            }
-            
-            if(curr==dest)
-                return ans;
-
-             for(int i=1;i<=6 && curr+i<=n*n;i++)
-            {
-                int next = curr+i;
-                if(path.find(next)!=path.end())
-                {
-                    if(path[next]<next && !visited[path[next]])
-                    {
-                           visited[path[next]] = true;
-                           q.push(path[next]);
-                    }
-                    else if(path[next]>next && !visited[path[next]])
-                    {
-                            visited[path[next]] = true;
-                            q.push(path[next]);
-                    }
-
-                }
-                 else
-                {
-                    if(!visited[next])
-                     {
-                         visited[next] = true;                       
-                         q.push(next);
-                     }
-                }
-
-            }
-                    
-                
-        }
-        
-        return -1;
-           
-    }
-        
-
     
     
     int snakesAndLadders(vector<vector<int>>& board) {
         
         int n = board.size();
+        queue<int> q;
+        q.push(1);
+        int ans = 0;
+        int goal = n*n;
         
-        unordered_map<int,int> path;
-        
-        int num  = 1;   
-        int flag = 0;
-        
-        for(int i=n-1;i>=0;i--)
-        {    
-            flag = flag ^ 1;
-            if(flag==1)
+        while(!q.empty())
+        {
+            ans++;
+            int siz = q.size();
+            
+            for(int i=0;i<siz;i++)
             {
-                for(int j=0;j<n;j++)
-                { 
-                    if(board[i][j]!=-1)
-                        path[num] = board[i][j];
-                    num++;
-                }
-                    
-            }
-            else
-            {
-                for(int j=n-1;j>=0;j--)
+                int curr = q.front();
+                q.pop();
+                
+                for(int i=1;i<=6;i++)
                 {
-                    if(board[i][j]!=-1)
-                        path[num] = board[i][j];
-                        num++;
-                }
+                    int y = curr+i;
                     
+                    if(y > goal)
+                        continue;
+                    
+                    int r = (y-1)/n;
+                    int c = (y-1)%n;
+                    
+//                     r = n-1-r;
+                    
+//                     if(r%2 == 1)
+//                         c = n-1-c;
+                    
+                    int &v = board[n-1-r][(r%2==0)?c:(n-1-c)];
+                    if(v == 0)
+                        continue;
+                    
+                    if(v > 0)
+                        y = v;
+                    
+                    if(y == goal)
+                        return ans;
+                    
+                    v = 0;
+                    
+                    q.push(y);
+                }
             }
         }
-
         
-        return No_Of_Moves(1,n*n,path,n);
+        
+        return -1;
+       
 
     }
-    
-    // https://leetcode.com/problems/snakes-and-ladders/discuss/173378/Diagram-and-BFS
-// see comment from shenglia
 };
 
