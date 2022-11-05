f08005de63f5e3526800edaa8a8b6af3162b1ac2
Swaraj Bhosle <66108123+Rio-321@users.noreply.github.com>
2022-11-05 12:03:16 +0530

    Added solution - LeetHub

---
+++ GCD of two numbers - GFG/gcd-of-two-numbers.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+	public:
+    int gcd(int A, int B) 
+	{ 
+	    if(A == B) return A;
+	    
+	    if(A > B) return gcd(A-B, B);
+	    else return gcd(A, B-A);
+	} 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+       	cout <<  ob.gcd(A, B) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
